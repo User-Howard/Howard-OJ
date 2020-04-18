44ea1cdd6b1cb70deb13a75833bf8a062e05f30b
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-04-18 20:05:35 +0800

    Update a003.cpp

--- AC/a003.cpp
+++ AC/a003.cpp
@@ -10,17 +10,14 @@ int main()
 	{
 		S=(M*2+D)%3;
 		if(S==0)
-		{
 			printf("普通");
-		}
+		
 		else if(S==1)
-		{
 			printf("吉");
-		}
+		
 		else if(S==2)
-		{
 			printf("大吉");
-		}
+		
 	}
 	return 0;
 }
