a39f7f43442709b7671682fcf4f5edc7cac89a84
User-Howard <howard20060216@gmail.com>
2020-04-25 13:57:29 +0800

    config

--- AC/a006.cpp
+++ AC/a006.cpp
@@ -10,17 +10,11 @@ int main(void)
 	rest=b*b-4*a*c;
 	
 	if(rest==0)
-	{
 		printf("Two same roots x=%d",(int)(-b/(2*a)));
-	}
 	else if(rest<0)
-	{
 		printf("No real root");
-	}
 	else
-	{
 		printf("Two different roots x1=%d , x2=%d",(int)(-b+ sqrt(rest))/(2*a), (int)(-b-sqrt(rest))/(2*a));
-	}
-	
+		
 	return 0;
 }
