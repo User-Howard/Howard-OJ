d7e09714e253dbd54de03601c296900b6dbaa91c
User-Howard <howard20060216@gmail.com>
2020-05-02 19:14:32 +0800

    push

--- NOT/e995.cpp
+++ NOT/e995.cpp
@@ -5,8 +5,7 @@
 
 using namespace std;
 
-string str(long long int a)
-{
+string str(long long int a){
     string Ans;
     stringstream s;
     s<<a;
@@ -14,8 +13,7 @@ string str(long long int a)
     return Ans;
 }
 
-int solve(int a)
-{
+int len(int a){
     // 9 90 900 9000 90000
     int i=0,rest=0;
     while(a != 0)
@@ -23,21 +21,43 @@ int solve(int a)
         a/=10;
         i++;
     }
-    cout<<i;
+    return i;
 }
 
-int main(void)
-{
+int main(void){
     
     string lstr="";
-    long long int N=9000000000,i=0;
-    solve(999);
-    while(scanf("%lld",&N) != EOF)
-    {
-        break;
+    long long int N,n,r;
+    long long int li[]={0,9,90,900,9000,90000,900000,9000000,90000000,900000000,9000000000};
+    int area=0;
+    while(scanf("%lld",&N) != EOF){
+        if(N<10)
+            printf("%d\n",N);
+        else{
+            r = 0;
+            for (int i = 0;; i++)
+            {
+                r += li[i];
+                area = i;
+                if (r + li[i + 1] >= N)
+                {
+                    break;
+                }
+            }
+            n=N-r;
+            int l;
+            
+            l=n/(area+1);
+            if(n%(area+1) != 0)
+            	l++;
+            
+            int Ans=l-1;
+            printf("area=%d r=%d l=%d\n",area,n,l);
+            cout<<str(Ans+pow(10,area))[(n-1)%(area+1)]<<endl;
+        }
+        
+        
     }
     
     return 0;
 }
-//123456789101112131415161718192021222324252627282930313233343536373839404142434445464748495051525354555657585960
-//99100101102103104105106107108109
