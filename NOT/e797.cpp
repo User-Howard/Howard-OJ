6bd73a294e01a074f841e23520e87e0071194e96
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-05-05 13:08:31 +0800

    Add files via upload

--- NOT/e797.cpp
+++ NOT/e797.cpp
@@ -2,12 +2,28 @@
 using namespace std;
 
 
-int main(void)
-{
+int main(void){
 	int x,y;
+	bool ist;
 	scanf("%d%d",&x,&y);
 	int arr[x][y];
-	for(int i=0;i<x;i++)
-		scanf("%d")
+	for(int i=0;i<x;i++){
+		for(int j=0;j<y:j++){
+			scanf("%d",&arr[i][j]);
+		}
+	}
+	
+	//AND
+	for(int i=0;i<x;i++){
+		ist = true;
+		for(int j=0;j<y;j++){
+			if(arr[x][y] == 0){
+				
+			}
+		}
+	}
+	//OR
+	
+	//XOR
 	return 0;
 }
