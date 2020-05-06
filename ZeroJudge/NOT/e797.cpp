eff401d6b31549cc1ae6fa071b8d712876fe7e96
User-Howard <howard20060216@gmail.com>
2020-05-07 00:33:08 +0800

    整理中

---
+++ ZeroJudge/NOT/e797.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+
+int main(void){
+	int x,y;
+	bool ist;
+	scanf("%d%d",&x,&y);
+	int arr[x][y];
+	for(int i=0;i<x;i++){
+		for(int j=0;j<y:j++){
+			scanf("%d",&arr[i][j]);
+		}
+	}
+	
+	//AND
+	for(int i=0;i<x;i++){
+		ist = true;
+		for(int j=0;j<y;j++){
+			if(arr[x][y] == 0){
+				
+			}
+		}
+	}
+	//OR
+	
+	//XOR
+	return 0;
+}
