d870897b98721198b8a67faae8df61c25eed5945
User-Howard <howard20060216@gmail.com>
2020-05-20 19:18:54 +0800

    空格題

--- ZeroJudge/AC/a520.cpp
+++ ZeroJudge/AC/a520.cpp
@@ -4,9 +4,8 @@ using namespace std;
 
 
 int main(void){
-    // ios_base::sync_with_stdio(false);
-    // cin.tie(NULL);
-    // cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
     
     string L;
     int count;
