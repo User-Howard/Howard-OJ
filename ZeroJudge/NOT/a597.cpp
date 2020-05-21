541af00a258cc5723079ea620f357a5c3933ad6e
User-Howard <howard20060216@gmail.com>
2020-05-21 23:11:27 +0800

    config

--- ZeroJudge/NOT/a597.cpp
+++ ZeroJudge/NOT/a597.cpp
@@ -1,37 +1,53 @@
 #include<iostream>
-#define TIMES 2
+#define TIMES 1
 using namespace std;
 
 
 int Xr,Yr;
 char arr[500][500];
-
+int limit=TIMES;
 
 bool check(int lx, int ly){
-    return;
-}
-bool de(int x, int y){
-    // TODO ↓判斷是否在迴圈裡
-    if((x<0)or(y<0)or(1))
-    if(arr[x][y] == 'X')
+     // TODO ↓判斷是否在迴圈裡
+    if((lx<0)or(ly<0)or(lx>Xr-1)or(ly>Yr-1))
         return 0;
+    else
+        return 1;
+}
+
+
+void de(int x, int y){
+cout<<arr[1][1]<<endl;
+cout<<"1";
+    if(!check(x, y))
+        return;
+    if(arr[x][y] == 'X')
+        return;
     else{
-        
-        /*
-        de(arr, x+1, y+0);
-        de(arr, x-1, y+0);
-        de(arr, x+0, y+1);
-        de(arr, x+0, y-1);
-        */
+        arr[x][y] = 'X';
+        de(x+1, y+0);
+        de(x-1, y+0);
+        de(x+0, y+1);
+        de(x+0, y-1);
+
     }
 }
 
 void solve() {
+    int maxim=0, pool=0;
     cin>>Xr>>Yr;
     for(int i=0;i<Xr;i++)
         for(int j=0;j>Yr;j++)
             cin>>arr[i][j];
-
+    
+    for(int i=0;i<Xr;i++){
+        for(int j=0;j<Yr;j++){
+            if(arr[i][j] == 'J'){
+                de(i, j);
+                pool++;
+            }
+        }
+    }
 
 }
 
@@ -40,7 +56,6 @@ int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int limit=TIMES;
     while(limit--)
         solve();
 
