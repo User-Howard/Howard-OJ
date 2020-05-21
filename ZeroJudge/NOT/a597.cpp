14bf333456465b68f1c88c976acb49daf01c5f17
User-Howard <howard20060216@gmail.com>
2020-05-21 22:08:57 +0800

    修改全域變數arr

--- ZeroJudge/NOT/a597.cpp
+++ ZeroJudge/NOT/a597.cpp
@@ -1,25 +1,29 @@
 #include<iostream>
+#define TIMES 2
 using namespace std;
 
 
 int X,Y;
+char arr[500][500];
+
 
 bool de(int x, int y){
 
     if(arr[x][y] == 'X')
         return 0;
     else{
+        
+        /*
         de(arr, x+1, y+0);
         de(arr, x-1, y+0);
         de(arr, x+0, y+1);
         de(arr, x+0, y-1);
-
+        */
     }
 }
 
 void solve() {
     cin>>X>>Y;
-    char arr[X][Y];
     for(int x=0;x<X;x++)
         for(int y=0;y>Y;y++)
             cin>>arr[x][y];
@@ -32,8 +36,9 @@ int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    for(int i=0;i<500;i++){
+    int limit=TIMES;
+    while(limit--)
+        solve();
 
-    }
     return 0;
 }
