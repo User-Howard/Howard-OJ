f1bf56c80af397a1c3f2898fa3b94f17b687a14b
User-Howard <howard20060216@gmail.com>
2020-05-23 12:53:13 +0800

    河邊

--- ZeroJudge/NOT/a597.cpp
+++ ZeroJudge/NOT/a597.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
-#define TIMES 1
+#include<algorithm>
+#define TIMES 500
 using namespace std;
 
 
-int Xr,Yr;
+int Xr, Yr, rest;
 char arr[500][500];
 int limit=TIMES;
 
 bool check(int lx, int ly){
-     // TODO ↓判斷是否在迴圈裡
-    if((lx<0)or(ly<0)or(lx>Xr-1)or(ly>Yr-1))
+    if((lx<0)or(ly<0)or(lx>=Xr)or(ly>=Yr))
         return 0;
     else
         return 1;
@@ -17,13 +17,14 @@ bool check(int lx, int ly){
 
 
 void de(int x, int y){
-cout<<arr[1][1]<<endl;
-cout<<"1";
+// cout<<arr[1][1]<<endl;
+// cout<<"1";
     if(!check(x, y))
         return;
     if(arr[x][y] == 'X')
         return;
     else{
+        rest++;
         arr[x][y] = 'X';
         de(x+1, y+0);
         de(x-1, y+0);
@@ -33,31 +34,32 @@ cout<<"1";
     }
 }
 
-void solve() {
-    int maxim=0, pool=0;
-    cin>>Xr>>Yr;
-    for(int i=0;i<Xr;i++)
-        for(int j=0;j>Yr;j++)
-            cin>>arr[i][j];
-    
-    for(int i=0;i<Xr;i++){
-        for(int j=0;j<Yr;j++){
-            if(arr[i][j] == 'J'){
-                de(i, j);
-                pool++;
-            }
-        }
-    }
-
-}
 
 
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+    
+    while(limit--){
+        int maxim=0, pool=0;
+        cin>>Xr>>Yr;
+        for(int i=0;i<Xr;i++)
+            for(int j=0;j<Yr;j++){
+                cin>>arr[i][j];
+            }
 
-    while(limit--)
-        solve();
+        for(int i=0;i<Xr;i++){
+            for(int j=0;j<Yr;j++){
+                rest=0;
+                if(arr[i][j] == 'J'){
+                    de(i, j);
+                    pool++;
+                    maxim=max(rest, maxim);
+                }
+            }
+        }
+        printf("%d %d\n",pool,maxim);
+    }
 
     return 0;
 }
