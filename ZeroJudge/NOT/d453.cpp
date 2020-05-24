9f14941d0f4b5ac0812623b7afd4e293518a6018
User-Howard <howard20060216@gmail.com>
2020-05-24 14:31:17 +0800

    Update d453.cpp

--- ZeroJudge/NOT/d453.cpp
+++ ZeroJudge/NOT/d453.cpp
@@ -2,10 +2,29 @@
 using namespace std;
 
 
+int Times;
+
+
+void solve(){
+    int sizeX, sizeY;
+    cin>>sizeX>>sizeY;
+    int rod[sizeX][sizeX]
+    bool puzzle[sizeX][sizeY];
+    for(int i=0;i<sizeX;i++){
+        for(int j=0;j<sizeY;j++){
+            cin>>puzzle[i][j];
+        }
+    }
+
+}
+
+
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    
+    cin>>Times;
+    while(Times--)
+        solve();
     return 0;
 }
