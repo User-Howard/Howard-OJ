78aa2d932d5206bfd7eafcf127480f07335e0773
User-Howard <howard20060216@gmail.com>
2020-05-24 23:24:08 +0800

    簡單

---
+++ ZeroJudge/AC/a054.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int li[26]={1,0,9,8,7,6,5,4,9,3,2,2,1,0,8,9,8,7,6,5,4,3,1,3,2,0};
+    int check;
+    string R;
+    while(cin>>R){
+        check=0;
+
+        check=R[0]-48;
+        for(int i=1;i!=8;i++)
+            
+        
+    }
+
+    return 0;
+}
