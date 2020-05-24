0fdc13ca63bfa270ebfb371218755d6355629903
User-Howard <howard20060216@gmail.com>
2020-05-24 15:52:28 +0800

    Update d453.cpp

--- ZeroJudge/NOT/d453.cpp
+++ ZeroJudge/NOT/d453.cpp
@@ -1,18 +1,33 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
 
 
 int Times;
+int sizeX, sizeY;
+int doc[102][102];
+bool puzzle[102][102];
+
+
+void bfs(int i, int j, int NOW){
+	if(puzzle[i][j]||i>=sizeX||i<0||j>=sizeY||j<0) return;
+    if(doc[i][j]);
+	doc[i][j]=NOW;
+	bfs(i+1, j, NOW+1);
+	bfs(i-1, j, NOW+1);
+	bfs(i, j+1, NOW+1);
+	bfs(i, j-1, NOW+1);
+	return;
+}
 
 
 void solve(){
-    int sizeX, sizeY;
     cin>>sizeX>>sizeY;
-    int rod[sizeX][sizeX]
-    bool puzzle[sizeX][sizeY];
+    memset(doc, 10000, sizeof(doc));
     for(int i=0;i<sizeX;i++){
         for(int j=0;j<sizeY;j++){
-            cin>>puzzle[i][j];
+            // cin>>puzzle[i][j];
+            printf("%d\n",doc[i][j]);
         }
     }
 
