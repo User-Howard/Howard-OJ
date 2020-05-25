2ad6e71ff57536616423e430a7c76656ba77e21d
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-05-25 21:29:47 +0800

    身分證

--- ZeroJudge/AC/a054.cpp
+++ ZeroJudge/AC/a054.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
+ 
     int li[26]={1,0,9,8,7,6,5,4,9,3,2,2,1,0,8,9,8,7,6,5,4,3,1,3,2,0};
     int check, sum;
     string R;
