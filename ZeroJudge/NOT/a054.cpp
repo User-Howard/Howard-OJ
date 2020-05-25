f392750905db2a991fc8c5adfd394dd376b432da
User-Howard <howard20060216@gmail.com>
2020-05-25 21:17:42 +0800

    接受變更

--- ZeroJudge/NOT/a054.cpp
+++ ZeroJudge/NOT/a054.cpp
@@ -11,20 +11,11 @@ int main(void){
     string R;
     while(cin>>R){
         check=0;
-<<<<<<< HEAD:ZeroJudge/AC/a054.cpp
 
         check=R[0]-48;
         for(int i=1;i!=8;i++)
-            $1
+            
         
-=======
-		sum=0;
-		
-        check=R[0]-'0';
-		for(int i=8; i != 1; i--)
-			sum += i * (R[8 - i] - '0');
-        // cout << (check - (sum % 10)) % 10 << '\n';
->>>>>>> 4b5098c9346b56bb768564a0a82520faa3c5299f:ZeroJudge/NOT/a054.cpp
     }
 
     return 0;
