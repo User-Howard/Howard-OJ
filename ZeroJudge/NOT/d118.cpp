73f46b4c0c11e421a1ee79c70130208ae852011f
User-Howard <howard20060216@gmail.com>
2020-05-28 00:00:56 +0800

    LAST

--- ZeroJudge/NOT/d118.cpp
+++ ZeroJudge/NOT/d118.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 bool visited[36000000];
 int need;
+char findi;
 string R;
 
 
@@ -11,6 +12,22 @@ int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    
+    while(cin>>R>>need){
+        findi='9';
+        while(need > 0){
+            for(int i=0;i<R.size();i++){
+                if(R[i] == findi){
+                    visited[i]=1;
+                    need--;
+                }
+                if(need == 0)
+                    break;
+            }
+            find--;
+        }
+        for(int i=0;i<R.size();i++){
+            
+        }
+    }
     return 0;
 }
