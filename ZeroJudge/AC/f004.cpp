e55e684055cb377adcc33899436eae3861c0607b
User-Howard <howard20060216@gmail.com>
2020-05-27 20:36:18 +0800

    Update f004.cpp

--- ZeroJudge/AC/f004.cpp
+++ ZeroJudge/AC/f004.cpp
@@ -1,65 +1,20 @@
-#include<iostream>
-#include<vector>
-using namespace std;
+#include<stdio.h>
 
-int MONEY[7]={1000,500,100,50,10,5,1};
-vector<int > ale(7,0);
-
-
-
-int main(void)
+int main()
 {
-	int N,rest;
-	while(scanf("%d",&N) != EOF){
-		
-		rest=N;
-		
-		for(int i=0;i<7;i++)
-			ale[i]=0;
-		
-		for(int i=0;i<7;i++)
-		{
-			while(rest>=MONEY[i])
-			{
-				ale[i]++;
-				rest-=MONEY[i];
-				//cout<<N<<" "<<MONEY[i]<<endl;
-			}
-		}
-		
-		int r=0;
-		for(int i=0;i<7;i++)
-			if(ale[i] != 0)
-				r++;
-		
-		printf("%d = ",N);
-		
-		for(int i=0;i<7;i++)
-		{
-			if(ale[i] != 0)
-			{
-				printf("%d*%d ",MONEY[i],ale[i]);
-				r--;
-				if(r >= 1)
-				{
-					printf("+ ");
-			
-				}
-
-			}
-				
-				
-		}
-		printf("\n");
+	int N=0,T=0;
+	scanf("%d%d",&N,&T);
+	int ans=0;
+	if(N!=1)
+	{
+		T/=N;
+		N/=N;
 	}
-	return 0;
+	int temp=T+N;
+	while(temp!=1)
+	{
+		temp/=2;
+		ans+=1;
+	}
+	printf("%d\n",ans);
 }
-/*
-1?¡M
-5?¡M
-10?¡M
-50?¡M
-100?¡M
-500?¡M
-1000?¡M
-*/
