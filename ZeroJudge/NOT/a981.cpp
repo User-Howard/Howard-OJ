d2682d94e2b5f8bd4e4993472c1f9c03ce3cb5db
User-Howard <howard20060216@gmail.com>
2020-05-29 23:00:52 +0800

    TLE

--- ZeroJudge/NOT/a981.cpp
+++ ZeroJudge/NOT/a981.cpp
@@ -3,10 +3,37 @@
 using namespace std;
 
 
-int N, M;
-int arr[30];
+int N, M, nowlimit;
+int list[30], arr[30];
+bool visit[30], f;
 
-void BFS(){
+int SUM(){
+	int sum=0;
+	for(int i=0;i<nowlimit;i++)
+		sum+=arr[i];
+	return sum;
+}
+void BFS(int x, int T){
+	if(T>nowlimit){
+		if(SUM() == M){
+			f=false;
+			for(int i=0;i<nowlimit;i++){
+				cout<<arr[i]<<" ";
+			}
+			cout<<endl;
+		}
+
+	}
+	else{
+		for(int i=x;i<N;i++){
+			if(visit[i])
+				continue;
+			arr[T-1]=list[i];
+			visit[i] = 1;
+			BFS(i+1, T+1);
+			visit[i] = 0;
+		}
+	}
 	return;
 }
 
@@ -15,7 +42,21 @@ int main(void){
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	
+	cin>>N>>M;
+	for(int i=0;i<N;i++)
+		cin>>list[i];
 	
+	sort(list,list+N);
+	f=true;
+	for(int i=0;i<N;i++){
+		arr[0]=list[i];
+		visit[i] = 1;
+		BFS(i, 2);
+		
+		visit[i] = 0;
+	}
+	if(f)
+		cout<<"-1"<<endl;
 	return 0;
 }
 /*
