5d5c14981f818ca7322bf358eccea2b242b69ef1
User-Howard <howard20060216@gmail.com>
2020-05-29 23:27:22 +0800

    TLE

--- ZeroJudge/NOT/a981.cpp
+++ ZeroJudge/NOT/a981.cpp
@@ -7,12 +7,46 @@ int N, M, nowlimit;
 int list[30], arr[30];
 bool visit[30], f;
 
-int SUM(){
-	int sum=0;
-	for(int i=0;i<nowlimit;i++)
-		sum+=arr[i];
-	return sum;
+
+void BFS(int x, int T, int sum){
+	for(int i=x;i<N;i++){
+		if(visit[i])
+			continue;
+		arr[T-1]=list[i];
+		visit[i] = 1;
+		BFS(i+1, T+1, 0);
+		visit[i] = 0;
+	}
+	
+	return;
 }
+
+
+int main(void){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin>>N>>M;
+	for(int i=0;i<N;i++)
+		cin>>list[i];
+	sort(list,list+N);
+
+	f=true;
+	
+	
+	return 0;
+}
+/*
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+
+int N, M, nowlimit;
+int list[30], arr[30];
+bool visit[30], f;
+
+
 void BFS(int x, int T){
 	if(T>nowlimit){
 		if(SUM() == M){
@@ -41,42 +75,23 @@ void BFS(int x, int T){
 int main(void){
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	
+
 	cin>>N>>M;
 	for(int i=0;i<N;i++)
 		cin>>list[i];
-	
 	sort(list,list+N);
+
 	f=true;
 	for(int i=0;i<N;i++){
 		arr[0]=list[i];
 		visit[i] = 1;
-		BFS(i, 2);
-		
+		for(nowlimit=N; nowlimit != 0;nowlimit--){
+			BFS(i, 2);
+		}
 		visit[i] = 0;
 	}
 	if(f)
 		cout<<"-1"<<endl;
-	return 0;
-}
-/*
-#include<iostream>
-#include<algorithm>
-using namespace std;
-int N,M,r;
-
-
-
-
-int main(void){
-	while(scanf("%d %d",&N,&M) != EOF){
-		for(int i=0;i<N;i++){
-			scanf("%d",&r);
-		}
-		sort(arr.begin(),arr.end());
-		
-		
-	}
 	return 0;
 }
 */
