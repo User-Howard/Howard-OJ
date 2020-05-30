2c528515aadc9d152d918e3000f8aed347096549
User-Howard <howard20060216@gmail.com>
2020-05-30 18:21:53 +0800

    Update 1004.cpp

--- TIOJ/1004.cpp
+++ TIOJ/1004.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<string.h>
 #define PASS while(arr[r%N] == 0)r+=1;
 using namespace std;
 
@@ -7,27 +6,22 @@ int N, arr[50];
 int Lastpeople(){
     int sum=0;
     for(int i=0;i<N;i++){
-        // cout<<arr[i]<<" ";
         sum+=(arr[i] == 1);
     }
     return sum;
 }
 
 int main(void){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    cin>>N;
+    scanf("%d",&N);
     for(int i=0;i<N;i++)
         arr[i]=1; 
-
-    for(int r=1;Lastpeople() != 1;r++){
+    for(int r=1, i=1; i<N ;r++, i++){
         PASS
         arr[r%N]=0;
         PASS
+        cout<<"e"<<endl;
     }
     
-    // 輸出
     for(int i=0;i<N;i++)
         if(arr[i] == 1)
             cout<<i+1<<endl;
