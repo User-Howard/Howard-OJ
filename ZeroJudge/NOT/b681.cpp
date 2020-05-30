c96741c340f86072ba1db6f6a4fd699b13c22120
User-Howard <howard20060216@gmail.com>
2020-05-30 23:05:41 +0800

    E

--- ZeroJudge/NOT/b681.cpp
+++ ZeroJudge/NOT/b681.cpp
@@ -1,1 +1,16 @@
-//COOL
+#include<iostream>
+using namespace std;
+
+
+int main(void){
+    int N, Ans;
+    scanf("%d",&N);
+    if(N>0){
+        Ans=(N*2)-1;
+    }
+    else{
+        Ans=(-N*2);
+    }
+    printf("%d\n",Ans);
+    return 0;
+}
