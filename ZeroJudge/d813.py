0ebba1c06f1b4902d6106d1c63f2b0d2cfb07ef7
User-Howard <howard20060216@gmail.com>
2020-05-31 12:05:34 +0800

    -

---
+++ ZeroJudge/d813.py
@@ -0,0 +1,31 @@
+Case=1
+while True:
+    try:
+        n,m=[int(i) for i in input().split()]
+    except EOFError:
+        break
+    if(n==0 and m==0):
+        break;
+    r=1
+    arr=[-1 for i in range(n)]
+    for i in range(m):
+        a,b=[int(i) for i in input().split()]
+        if(arr[a-1]==-1 and arr[b-1]==-1):
+            arr[a-1],arr[b-1]=r,r
+            r+=1
+        elif(arr[a-1]!=-1):
+            arr[b-1]=arr[a-1]
+
+
+    visit=[0 for i in range(n)]
+    S=0
+    for i in arr:
+        if(i == -1):
+            S+=1
+            continue
+        if(visit[i-1] == 1):
+            continue
+        visit[i-1]=1;
+        S+=1
+    print(S)
+    
