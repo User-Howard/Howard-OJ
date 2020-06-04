6f8d5eace193f3d2b23a1d9398860f5f09c7059c
User-Howard <howard20060216@gmail.com>
2020-06-04 20:17:08 +0800

    config

--- ZeroJudge/AC/d453.cpp
+++ ZeroJudge/AC/d453.cpp
@@ -5,11 +5,22 @@ using namespace std;
 
 int Times;
 int sizeX, sizeY;
-int sx, sy;
-int ex, ey;
+int sx, sy, ex, ey;
 int doc[102][102];
 bool puzzle[102][102];
+void bfs(int, int, int);
+void solve();
 
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin>>Times;
+    while(Times--)
+        solve();
+    return 0;
+}
 void bfs(int i, int j, int NOW){
 	if(puzzle[i][j]||i>=sizeX||i<0||j>=sizeY||j<0) return;
 
@@ -23,8 +34,6 @@ void bfs(int i, int j, int NOW){
     }
     return;
 }
-
-
 void solve(){
     cin>>sizeX>>sizeY>>sx>>sy>>ex>>ey;
     sx--,sy--,ex--,ey--;
@@ -35,27 +44,9 @@ void solve(){
         string r;
         cin>>r;
         for(int j=0;j<sizeY;j++){
-            puzzle[i][j]=(r[j] == '1');\
+            puzzle[i][j]=(r[j] == '1');
         }
     }
     bfs(sx, sy, 1);
-    // for(int i=0;i<sizeX;i++){
-    //     for(int j=0;j<sizeY;j++){
-    //         if(puzzle[i][j])cout<<" "<<"\t";
-    //         else cout<<doc[i][j]<<"\t";
-    //     }
-    //     cout<<endl;
-    // }
     cout<<doc[ex][ey]<<endl;
-}
-
-
-int main(void){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    cin>>Times;
-    while(Times--)
-        solve();
-    return 0;
 }
