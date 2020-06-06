5547dafadadc6772c5d2d7d35bd2ff4bb125de6f
User-Howard <howard20060216@gmail.com>
2020-06-06 12:59:34 +0800

    測試

---
+++ Others/analysis.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#define LEN arr.size()
+using namespace std;
+
+
+int main(void){
+    int N;
+    vector<int >arr;
+    scanf("%d",&N);
+    for(int i=0,r;i<N;i++){
+        scanf("%d",&r);
+        arr.push_back(r);
+    }
+    int l=0;
+    while(l<=LEN){
+
+    }
+    return 0;
+}
