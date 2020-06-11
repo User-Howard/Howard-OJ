772a005bd5e97385413058f1aadfc84ba36a1238
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-06-11 12:59:55 +0800

    Create c290.cpp

---
+++ ZeroJudge/NOT/c290.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+using namespace std;
+
+
+int N, M, s1, s2, arr[802];
+bool find(int N){
+
+    return 1;
+}
+int main(void){
+    while(scanf("%d %d", &N, &M) == 2){
+        memset(arr, -1, sizeof(arr));
+        while(scanf("%d %d",&s1, &s2) == 2){
+            arr[s2] = s1;
+        }
+
+    }
+
+    return 0;
+}
