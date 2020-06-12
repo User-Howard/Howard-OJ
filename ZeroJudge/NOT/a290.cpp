83d2babc720fa9cebe2fa0eac065191dc92d492e
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-06-12 13:10:47 +0800

    Update a290.cpp

--- ZeroJudge/NOT/a290.cpp
+++ ZeroJudge/NOT/a290.cpp
@@ -1,22 +1,51 @@
 #include<iostream>
+#include<set>
 #include<stdio.h>
 #include<string.h>
 using namespace std;
 
 
-int N, M, s1, s2, arr[802];
-bool find(int N){
+int N, M, s1, s2, start, goal;
+set<int >arr[802];
+int visited[802];
+bool dfs(int );
 
-    return 1;
-}
+
 int main(void){
     while(scanf("%d %d", &N, &M) == 2){
-        memset(arr, -1, sizeof(arr));
-        while(scanf("%d %d",&s1, &s2) == 2){
-            arr[s2] = s1;
+        memset(visited, -1, sizeof(visited));
+        for(int i=0;i<N;i++){
+            arr[i].clear();
         }
-        cout<<arr[3];
+
+        for(int i=0;i<M;i++){
+            scanf("%d %d", &s1, &s2);
+            arr[s1].insert(s2);
+        }
+
+        scanf("%d %d", &start, &goal);
+        if(dfs(start) == 1){
+            cout<<"Yes!!!\n";
+        }
+        else{
+            cout<<"No!!!\n";
+        }
     }
 
+    return 0;
+}
+bool dfs(int N){
+    if(arr[N].count(goal))
+        return 1;
+    if(arr[N].count(-1) || visited[N] == 1)
+        return 0;
+    else{
+        for(set<int>::iterator it=arr[N].begin(); it!=arr[N].end(); ++it){
+            visited[*it]=1;
+            if(dfs(*it))
+                return 1;
+            visited[*it]=0;
+        }
+    }
     return 0;
 }
