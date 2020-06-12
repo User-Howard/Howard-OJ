e88d0ca8a6312aa7d5de200b650b0b029ea597a5
User-Howard <howard20060216@gmail.com>
2020-06-12 16:50:26 +0800

    簡單dfs

--- ZeroJudge/AC/a290.cpp
+++ ZeroJudge/AC/a290.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<set>
 #include<stdio.h>
-#include<string.h>
 using namespace std;
 
 
@@ -13,11 +12,10 @@ bool dfs(int );
 
 int main(void){
     while(scanf("%d %d", &N, &M) == 2){
-        memset(visited, -1, sizeof(visited));
         for(int i=0;i<N;i++){
             arr[i].clear();
+            visited[i]=0;
         }
-
         for(int i=0;i<M;i++){
             scanf("%d %d", &s1, &s2);
             arr[s1].insert(s2);
@@ -25,27 +23,20 @@ int main(void){
 
         scanf("%d %d", &start, &goal);
         if(dfs(start) == 1){
-            cout<<"Yes!!!\n";
+            puts("Yes!!!\n");
         }
         else{
-            cout<<"No!!!\n";
+            puts("No!!!\n");
         }
     }
 
     return 0;
 }
 bool dfs(int N){
-    if(arr[N].count(goal))
-        return 1;
-    if(arr[N].count(-1) || visited[N] == 1)
-        return 0;
-    else{
-        for(set<int>::iterator it=arr[N].begin(); it!=arr[N].end(); ++it){
-            visited[*it]=1;
-            if(dfs(*it))
-                return 1;
-            visited[*it]=0;
-        }
+    visited[N] = 1;
+    for(set<int>::iterator it=arr[N].begin(); it!=arr[N].end(); ++it){
+        if(visited[*it] != 1 and (*it == goal || dfs(*it)))
+            return 1;
     }
     return 0;
 }
