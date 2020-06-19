e12d6d53a22c536e3b36fa1bbe1febf5df9b18af
User-Howard <howard20060216@gmail.com>
2020-06-19 22:56:18 +0800

    Update b304.cpp

--- ZeroJudge/NF/b304.cpp
+++ ZeroJudge/NF/b304.cpp
@@ -1,1 +1,36 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
 
+
+int W, H;
+int arr[102][22];
+string rest;
+
+int dfs(int N){
+    if(N>-1 and )
+}
+
+
+int main(void){
+    while(scanf("%d %d", &W, &H) == 2){
+        getchar();
+        for(int i=0;i<H;i++){
+            getline(cin, rest);
+            for(int j=0;j<W*2-1;j++){
+                arr[i][j]=rest[j];
+            }
+        }
+
+        /*
+        cout<<"da..."<<endl;
+        for(int i=0;i<H;i++){
+            for(int j=0;j<W*2-1;j++){
+                cout<<(char)arr[i][j];
+            }
+            cout<<endl;
+        }
+        */
+    }
+    return 0;
+}
