a76993265c56d19681f0de9c1aa9fbeae14aaf84
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-06-22 13:01:56 +0800

    Update b304.cpp

--- ZeroJudge/NF/b304.cpp
+++ ZeroJudge/NF/b304.cpp
@@ -1,1 +1,26 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
 
+
+int N;
+string str;
+int countl(char );
+
+
+int main(void){
+    scanf("%d", &N);
+    getchar();
+    while(N--){
+        getline(cin, str);
+        cout<<countl();    
+    }
+    return 0;
+}
+int countl(char c){
+    int sum=0;
+    for(int i=0;i<str.size();i++){
+        sum+=str[i]==c;
+    }
+    return sum;
+}
