045fe7faa2250f53141a1b09243ecd0aa3490791
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-06-25 20:50:41 +0800

    Update e811.cpp

--- ZeroJudge/NF/e811.cpp
+++ ZeroJudge/NF/e811.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
-#define MAX 10000
+#define MAXL 10000
+#define MAXN 1200000000
 using namespace std;
 
 
 int main(void){
-    int arr, P, Q, R, A0, A1, N;
-    scanf("%d%d%d%d%d%d", &P, &Q, &R, &A0, &A1, &N);
+    int arr[MAXN], P, Q, R, N;
+    scanf("%d%d%d%d%d%d", &P, &Q, &R, &arr[0], &arr[1], &N);
     for(int i=2;i!=N+1;i++)
-
+        arr[i]=(P*arr[i-1] +Q*arr[i-2] +R)%MAXL;
+    printf("%d\n", arr[N]);
 }
