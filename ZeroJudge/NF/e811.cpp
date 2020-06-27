f4cbe288e5b01339bcdf2be018225f5b5dc97d25
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-06-27 16:38:56 +0800

    Update e811.cpp

--- ZeroJudge/NF/e811.cpp
+++ ZeroJudge/NF/e811.cpp
@@ -3,15 +3,16 @@
 using namespace std;
 
 
-int Ans, P, Q, a[2], R, N;
-int bbvc(int i, int j, int x){
+int Ans, P, Q, a[2], R;
+long long int N;
+int bbvc(int i, int j, long long int x){
     if(x==N+1)
         return j;
     else
         return bbvc(j, (P*j+Q*i+R)%MAXL, x+1);
 }
 int main(){  
-    scanf("%d%d%d%d%d%d", &P, &Q, &R, &a[0], &a[1], &N);
+    scanf("%d%d%d%d%d%lld", &P, &Q, &R, &a[0], &a[1], &N);
     Ans=N<2? a[N] : bbvc(a[0], a[1], 2);
     if(Ans<10)
         printf("000");
