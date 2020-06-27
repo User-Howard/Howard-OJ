32af73b2280532967f6293ee92d37c0c2baafdf5
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-06-27 14:30:49 +0800

    Update e811.cpp

--- ZeroJudge/NF/e811.cpp
+++ ZeroJudge/NF/e811.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 #define MAXL 10000
-#define MAXN 1200000000
 using namespace std;
 
 
-int P, Q, a1, a2, R, N;
+int P, Q, a[2], R, N;
 int bbvc(int i, int j, int x){
-    if(x==N)
-        return (i+j)%MAXL;
+    if(x==N+1)
+        return j;
     else
-        return bbvc(j, (i+j)%MAXL, x+1);
-int main(void){
-   
-    scanf("%d%d%d%d%d%d", &P, &Q, &R, &a1, &a2, &N);
-    printf("%d\n", bbvc());
+        return bbvc(j, (P*j+Q*i+R)%MAXL, x+1);
+}
+int main(){  
+    scanf("%d%d%d%d%d%d", &P, &Q, &R, &a[0], &a[1], &N);
+    printf("%d\n", N<2? a[N] : bbvc(a[0], a[1], 2) );
+    
 }
