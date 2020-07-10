29835392f663ac743aa01a169a2733f15dacb62f
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-07-10 20:50:31 +0800

    Update b304.cpp

--- ZeroJudge/NF/b304.cpp
+++ ZeroJudge/NF/b304.cpp
@@ -5,22 +5,66 @@ using namespace std;
 
 int N;
 string str;
-int countl(char );
-
+bool solve(string str){
+    if(( countl(str, '[') != countl(str, ']') ) or ( countl(str, '(') != countl(str, ')') )){
+        return 0;
+    }
+    else{
+        int smc=0, bic=0;
+        for(int i=0;i<str.size();i++){
+            if(str[i] == '('){
+                string rest = cut(str, i+1, n_find(str, i+1, ')'));
+                if(countl(rest, '[')!=countl(rest, ']'))
+                    return 0;
+                smc++;
+            }
+            else if(str[i] == ')')
+                smc--;
+            else if(str[i] == '['){
+                string rest = cut(str, i+1, n_find(str, i+1, ']'));
+                if(countl(rest, '(')!=countl(rest, ')'))
+                    return 0;
+                bic++;
+            }
+            else if(str[i] == ']')
+                bic--;
+            
+            if(bic<0 or smc<0){
+                return 0;
+            }
+            
+        }
+    }
+}
+int countl(string str, char c){
+    int sum=0;
+    for(int i=0;i<str.size();i++){
+        sum+=(str[i]==c);
+    }
+    return sum;
+}
+int n_find(string str, int latt, char c){
+    for(int i=latt;i<str.size();i++){
+        if(str[i] == c){
+            return i;
+        }
+    }
+    return -1;
+}
+string cut(string str, int x, int y){
+    string RET="";
+    for(int i=x;i<y;i++){
+        RET+=str[i];
+    }
+    return RET;
+}
 
 int main(void){
     scanf("%d", &N);
     getchar();
     while(N--){
         getline(cin, str);
-        cout<<countl();    
+        solve(str);
     }
     return 0;
-}
-int countl(char c){
-    int sum=0;
-    for(int i=0;i<str.size();i++){
-        sum+=str[i]==c;
-    }
-    return sum;
 }
