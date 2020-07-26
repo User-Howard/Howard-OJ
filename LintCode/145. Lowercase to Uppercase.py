256d806bc0bf6ec38bdfd43e94b4df47958ab01e
User-Howard <howard20060216@gmail.com>
2020-07-26 19:38:03 +0800

    Create 145. Lowercase to Uppercase.py

---
+++ LintCode/145. Lowercase to Uppercase.py
@@ -0,0 +1,9 @@
+class Solution:
+    """
+    @param character: a character
+    @return: a character
+    """
+
+    def lowercaseToUppercase(self, character):
+        # write your code here
+        return character.upper()
