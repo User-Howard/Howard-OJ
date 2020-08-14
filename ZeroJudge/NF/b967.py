542c875c8024efd74ba77fd3383710a359243e57
User-Howard <howard20060216@gmail.com>
2020-08-15 00:51:57 +0800

    F

--- ZeroJudge/NF/b967.py
+++ ZeroJudge/NF/b967.py
@@ -1,6 +1,24 @@
 N = int(input())
-INF = [set(input().split()) for i in range(N-1)]
+INF = [[int(toint) for toint in input().split()] for i in range(N-1)]
 
-print(INF)
-def dfs(step=0):
-  pass
+def eraseOnList(l, i):
+    l.remove(i)
+    return l
+
+def road(x, se):
+    for i in (0, 1):
+        if se[i] == x:
+            return se[1-i]
+    return ;
+
+def dfs(x, state, step=0):
+    ext = [i for i in state if(x in i)]
+    # print(f"Call({x}) -> ext={ext}")
+    maxstep = step
+    for i in ext:
+        # print('\t'*step, road(x, i), state, step+1)
+        maxstep = max(maxstep, dfs(road(x, i), eraseOnList(state, i), step+1))
+    return maxstep
+
+
+print(max([dfs(i, INF) for i in range(N)]))
