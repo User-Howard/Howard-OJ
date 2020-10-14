90ba310880b8638e9876c8d5dd4c431e225970e0
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-10-14 20:22:47 +0800

    Update 3. 定時 K 彈NF.cpp

--- APCS/105 10/3. 定時 K 彈NF.cpp
+++ APCS/105 10/3. 定時 K 彈NF.cpp
@@ -1,21 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+const int MAXN = 2e5;
 int N, M, K;
+int arrb[MAXN];
+int overPerson(int x, int step){
+    int counter=0;
+    for(int i=1;i<N*step;i++){
+        if(arrb[(x+i)%N] == 1)
+            counter++;
+        if(counter == step)
+            return (x+i)%N;
+    }
+    return -1;
+}
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
     cin>>N>>M>>K;
-    int pointer=-1;
-
-    for(int i=N;i>N-K;i--){
-        pointer = (pointer+M)%i;
+    for(int i=0;i<N;i++){
+        arrb[i] = 1;
     }
-    printf("%d %d %d \n", pointer, K*N, K*M);
-    pointer = (pointer + K*N - K*M)%N;
-    cout<<pointer;
+    int point=-1;
+    while(K--){
+        point = overPerson(point, M);
+        arrb[point] = 0;
+    }
+    cout<<overPerson(point, 1)+1<<endl;
+
     return 0;
 }
-// °Ñ¦Ò¦Û : http://yuihuang.com/zj-c296/
