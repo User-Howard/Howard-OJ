2cecb45ce21887f929b0f8de646a2a13029e660c
User-Howard <howard20060216@gmail.com>
2020-10-15 23:28:45 +0800

    Update a982.cpp

--- ZeroJudge/NF/a982.cpp
+++ ZeroJudge/NF/a982.cpp
@@ -6,6 +6,17 @@ using namespace std;
 int N;
 char arr[102][102];
 queue<pair<int, int>>que;
+
+void bfs(int x, int y){
+    if(x>0)
+        que.push(make_pair(x-1, y));
+    else if(y>0)
+        que.push(make_pair(x, y-1));
+    else if(x<N-1)
+        que.push(make_pair(x+1, y));
+    else if(y<N-1)
+        que.push(make_pair(x, y+1));
+}
 int main(){
     scanf("%d", &N);
     for(int i=0;i<N;i++){
@@ -14,6 +25,7 @@ int main(){
         }
     }
 
+    for
 
     return 0;
 }
