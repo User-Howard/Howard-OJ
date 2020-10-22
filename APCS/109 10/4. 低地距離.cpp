233ff47a337f68ef00d120b07ef84439be51df7f
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-10-22 13:07:31 +0800

    Add files via upload

---
+++ APCS/109 10/4. 低地距離.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+usong namespace std;
+
+
+int main(){
+    long int N;
+    int arr[(int )2e5];
+    short int conc[(int )ie5];
+    fill(conc, conc+N, -1);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+
+    }
+    return 0;
+}
