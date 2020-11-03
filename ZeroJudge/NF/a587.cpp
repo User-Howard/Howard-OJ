43159a1c5554447d68e3e32e68fb09254b63dd33
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-11-03 13:08:34 +0800

    Update a587.cpp

--- ZeroJudge/NF/a587.cpp
+++ ZeroJudge/NF/a587.cpp
@@ -1,18 +1,25 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 
+int N, W, h=0;
+int arr[102];
+int dp[102];
+int f(int x){
+    
+}
 int main(){
-  ios::sync_with_stdio(false);
-  cin.tie(NULL);
-  
-  int N;
-  int arr[2000];
-  int vala[2000];
-  cin>>N;
-  
-  for(int i=0;i<N;i++){
-    cin>>arr[i]>>vala[i];
-  }
-  
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin>>N;
+    for(int i=0;i<N;i++){
+        int tempA, tempB;
+        cin>>tempA>>tempB;
+        
+        
+    }
+    cin>>W;
+    cout<<f(W)<<'\n';
 }
