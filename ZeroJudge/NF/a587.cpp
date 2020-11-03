0cb4593e5af3fa20bb1958a1ccc30db70290b67d
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-11-03 12:34:38 +0800

    Create a587.cpp

---
+++ ZeroJudge/NF/a587.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  int N;
+  int arr[2000];
+  int vala[2000];
+  cin>>N;
+  
+  for(int i=0;i<N;i++){
+    cin>>arr[i]>>vala[i];
+  }
+  
+}
