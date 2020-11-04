60eb208a20684bcee383869a5a5c90f32a7b0bcd
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-11-04 13:12:15 +0800

    Update a587.cpp

--- ZeroJudge/NF/a587.cpp
+++ ZeroJudge/NF/a587.cpp
@@ -1,13 +1,26 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
+#define INF (int )0x3f3f3f3f
 
-
-int N, W, h=0;
-int arr[102];
+int N, V;
+struct E{
+    int W, V;
+} arr[102];
 int dp[102];
 int f(int x){
-    
+    if(x<=0)
+        return 0;
+        /*
+    if(dp[x] > 0)
+        return dp[x];
+        */
+    int q = -INF;
+    for(int i=0;i<N;i++){
+        q = max(q, f(x - arr[i].W) + arr[i].V);
+    }
+    dp[x] = q;
+    return q;
 }
 int main(){
     ios::sync_with_stdio(false);
@@ -15,11 +28,11 @@ int main(){
 
     cin>>N;
     for(int i=0;i<N;i++){
-        int tempA, tempB;
-        cin>>tempA>>tempB;
-        
-        
+        cin>>arr[i].W>>arr[i].V;
     }
-    cin>>W;
-    cout<<f(W)<<'\n';
+    cin>>V;
+    cout<<f(V)<<'\n';
+    for(int i=1;i<=V;i++){
+        cout<<dp[i]<<' ';
+    }
 }
