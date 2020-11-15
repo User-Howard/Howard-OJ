021755bc04a63e0465666002da2d5bd1c4fc1ba1
User-Howard <howard20060216@gmail.com>
2020-11-15 16:05:50 +0800

    TODO

---
+++ TIOJ/1007_NF.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#define FOR(i, x, y) for(int i=x;i<y;i++)
+#define MSIZE (int )9e2
+#define INF 0x3f3f3f3f
+using namespace std;
+
+int N, M;
+int dp[MSIZE+2];
+int light(int x){
+    
+}
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
