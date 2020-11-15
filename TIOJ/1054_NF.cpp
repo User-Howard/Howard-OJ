0c883aa886aea2e7c2d1f90fd84fd55634a0eb1b
User-Howard <howard20060216@gmail.com>
2020-11-15 16:38:49 +0800

    TODO

---
+++ TIOJ/1054_NF.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#define FOR(i, x) for(int i=0;i<x;i++)
+using namespace std;
+
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+    return 0;
+}
