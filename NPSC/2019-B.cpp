c7254970dfca160ec1e77b87887a12e05e5fb105
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-12-02 13:05:41 +0800

    Create 2019-B.cpp

---
+++ NPSC/2019-B.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int N, K, P;
+vector<int >arr;
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cin>>N>>K>>P;
+	for(int i=0, temp;i<N;i++){
+		cin>>temp;
+		arr.push_back(temp);
+	}
+	
+	while(cin>>){
+		
+	}
+    return 0;
+}
