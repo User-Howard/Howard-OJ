7b35c6fa1bf8a0301dbe538cfd8a1071dcc8df4b
User-Howard <howard20060216@gmail.com>
2020-12-06 21:09:21 +0800

    Create S.cpp

---
+++ NPSC/S.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+struct E{
+    int num=1;
+    char c;
+}
+bool is_number(char c){
+    return '0'<=c and c<='9';
+}
+vector<E > restring(string& S){
+    vector<E >sarr;
+    for(int i=0;i<S.size();i++){
+        if()
+    }
+}
+int main(){
+    string S="1;
+
+    return 0;
+}
