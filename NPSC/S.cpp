8c14ad42b2fd826a975cc823e12a25f43d438dd7
User-Howard <howard20060216@gmail.com>
2020-12-07 23:18:13 +0800

    Update S.cpp

--- NPSC/S.cpp
+++ NPSC/S.cpp
@@ -7,16 +7,35 @@ struct E{
     int num=1;
     char c;
 };
-bool is_number(char c){
-    return '0'<=c and c<='9';
-}
 vector<E > restring(string& S){
     vector<E >sarr;
     for(int i=0;i<S.size();i++){
-        if(1);
+        E temp;
+        if(isdigit(S[i])){
+            temp.num = S[i] - '0';
+            temp.c = S[i+1];
+            i++;
+        }
+        else{
+            temp.c = S[i];
+        }
+        sarr.push_back(temp);
     }
+    return sarr;
 }
 int main(){
-    string S="1";
+    string S("");
+    cin>>S;
+    vector<E >arr;
+    arr = restring(S);
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i].num<<" -> "<<arr[i].c<<endl;
+    }
+    for(auto i:arr){
+        for(int j=0;j<i.num;j++){
+            cout<<i.c;
+        }
+    }
+    cout<<endl;
     return 0;
 }
