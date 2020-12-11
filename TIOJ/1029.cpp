047ccb413bd57c7443b9372916774adeb109e2d1
User-Howard <howard20060216@gmail.com>
2020-12-11 23:52:23 +0800

    Update 1029.cpp

--- TIOJ/1029.cpp
+++ TIOJ/1029.cpp
@@ -3,25 +3,27 @@
 #include<string.h>
 using namespace std;
 const int INF=0x3f3f3f3f;
-const int MAXN=(int )1e4+5;
+const int MAXN=(int )1e4+10;
 
 int N;
 int nums[MAXN];
 int prefix[MAXN];
 int dp[MAXN][MAXN];
 int The_sum_of(int l, int r){
-    return prefix[r-1]-prefix[l-1];
+    return prefix[r+1]-prefix[l];
 }
 int Game(int l, int r){
-    if(dp[l][r]!=-INF){
+    // printf("Call %d %d\n", l, r);
+    if(dp[l][r]>0){
         return dp[l][r];
     }
-    int q = max(0, 0);
-    return 0;
+    int q = max(The_sum_of(l+1, r)-Game(l+1, r)+nums[l], The_sum_of(l, r-1)-Game(l, r-1)+nums[r]);
+    dp[l][r] = q;
+    return q;
 }
 int main(){
     cin>>N;
-    memset(dp, 1, sizeof(dp));
+    memset(dp, 0, sizeof(dp));
     memset(prefix, 0, sizeof(prefix));
     for(int i=0;i<N;i++){
         cin>>nums[i];
@@ -31,12 +33,14 @@ int main(){
     for(int i=0;i<N;i++){
         dp[i][i] = nums[i];
     }
-    cout<<Game(0, N-1)<<endl;
-    for(int i=0;i<MAXN;i++){
-        for(int j=0;j<MAXN;j++){
+    cout<<Game(0, N-1)<<' '<<The_sum_of(0, N-1)-Game(0, N-1)<<endl;
+    /*
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
             cout<<dp[i][j]<<'\t';
         }
         cout<<endl;
     }
+    */
     return 0;
 }
