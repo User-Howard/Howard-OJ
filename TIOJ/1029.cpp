3359aefa4c1504a204b53bab8b5aa9f515d58453
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-12-11 13:08:12 +0800

    Update 1029.cpp

--- TIOJ/1029.cpp
+++ TIOJ/1029.cpp
@@ -1,23 +1,40 @@
 #include<iostream>
+#include<cmath>
+#include<string.h>
 using namespace std;
-const int MAXN=4;
-
+const int INF=0x3f3f3f3f;
+const int MAXN=(int )1e4+5;
 
 int N;
-int arr[1002];
-int dp[1002][1002];
+int nums[MAXN];
+int prefix[MAXN];
+int dp[MAXN][MAXN];
+int The_sum_of(int l, int r){
+    return -1;
+}
 int Game(int l, int r){
-    if(dp[l][r]!=INF){
+    if(dp[l][r]!=-INF){
         return dp[l][r];
     }
-    
+    int q = max(0, 0);
+    return 0;
 }
 int main(){
-    cout<<MAXN;
     cin>>N;
+    memset(dp, 1, sizeof(dp));
+    memset(prefix, 0, sizeof(prefix));
     for(int i=0;i<N;i++){
-        cin>>arr[i];
+        cin>>nums[i];
+    }
+    for(int i=0;i<N;i++){
+        dp[i][i] = nums[i];
     }
     cout<<Game(0, N-1)<<endl;
+    for(int i=0;i<MAXN;i++){
+        for(int j=0;j<MAXN;j++){
+            cout<<dp[i][j]<<'\t';
+        }
+        cout<<endl;
+    }
     return 0;
 }
