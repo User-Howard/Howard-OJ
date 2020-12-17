7f8efbe56872be77683e3980afd216c0aaeefd9a
User-Howard <60650989+User-Howard@users.noreply.github.com>
2020-12-17 13:12:20 +0800

    Update e287.cpp

--- e287.cpp
+++ e287.cpp
@@ -2,29 +2,26 @@
 #include<vector>
 using namespace std;
 
-struct E{
-        int way, sum;
-};
+
 int N, M;
 const int dx[4] = {0, -1, 1, 0};
 const int dy[4] = {1, 0, 0, -1};
 int arr[102][102];
 int road[102][102];
-bool cmp(E &a, E &b){
-    return a.sum < b.sum;
-}
 void dfs(int x, int y){
     printf("%d %d \n", x, y);
-    vector<E >mi;
+    int min_sum=-1, min_way;
     for(int i=0;i<4;i++){
         int nx = x+dx[i], ny = y+dy[i];
         if(nx<0||ny<0||nx==N||ny==M)
             continue;
-        mi.push_back(E(i, arr[x][y]));
+        if(arr[nx][ny]<min_sum){
+        	min_sum = arr[nx][ny];
+        	min_way = i;
+		}
     }
-    sort(mi.begin(), mi.bnd(), cmp);
     road[x][y] = true;
-    dfs(x+dx[mi[0].way], y+dy[mi[0].way);
+    dfs(x+dx[min_way], y+dy[min_way]);
 }
 int main(){
     int N;
