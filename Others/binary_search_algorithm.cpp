c224980e407cc53a082781a254ff99c8073aa55d
User-Howard <howard20060216@gmail.com>
2021-01-01 18:43:15 +0800

    debug int data[] into int data_arr[]

--- Others/binary_search_algorithm.cpp
+++ Others/binary_search_algorithm.cpp
@@ -5,19 +5,19 @@ using namespace std;
 #define mid (low+high)/2
 
 
-int data[100], val;
+int data_arr[100], val;
 int binary(int low, int high){
-    if(data[low] == val)
+    if(data_arr[low] == val)
         return low;
     else if(low>high)
         return -1;
 
-    if(val<data[mid]) {
-        printf("%d is between %d[%d] and %d[%d], find left\n", val, low, data[low], mid, data[mid]);
+    if(val<data_arr[mid]) {
+        printf("%d is between %d[%d] and %d[%d], find left\n", val, low, data_arr[low], mid, data_arr[mid]);
         high=mid-1;
     }
-    else if(val>data[mid]) {
-        printf("%d is between %d[%d] and %d[%d], find right\n", val, mid, data[mid], high, data[high]);
+    else if(val>data_arr[mid]) {
+        printf("%d is between %d[%d] and %d[%d], find right\n", val, mid, data_arr[mid], high, data_arr[high]);
         low=mid+1;
     }
     return binary(low, high);
@@ -25,12 +25,12 @@ int binary(int low, int high){
 
 int main(){
     for(int i=0,var=1; i<100; i++) {
-		data[i]=var;
+		data_arr[i]=var;
 		var+=rand()%5+1;
 	}
 	scanf("%d", &val);
     for(int j=0; j<100; j++) {
-        printf("%2d [%3d]  ", j, data[j]);
+        printf("%2d [%3d]  ", j, data_arr[j]);
     }
     printf("%d\n", binary(0, 99));
 
