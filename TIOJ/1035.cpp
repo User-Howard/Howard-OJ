02e0a127f2711e9e8532b7b515d2091833787554
User-Howard <howard20060216@gmail.com>
2021-01-23 20:02:53 +0800

    簡單字串分析

--- TIOJ/1035.cpp
+++ TIOJ/1035.cpp
@@ -1,32 +1,35 @@
 #include<iostream>
+#include<cmath>
 #include<vector>
 using namespace std;
 
 
-const int MAX_STRING_SIZE = 5e2 +2;
-vector<vector<bool> >dp(MAX_STRING_SIZE, vector<bool>(MAX_STRING_SIZE));
 int main(){
     ios::sync_with_stdio(NULL);
     cin.tie(0);
 
     string str1, str2;
     cin>>str1>>str2;
-    const int strlen = str1.size();
-    // init
-    for(int i=0;i<strlen;i++){
-        dp[0][i] = str1[0]==str2[i];
-        dp[i][0] = str1[i]==str2[0];
-    }
-    for(int i=1;i<strlen;i++){
-        for(int j=1;j<strlen;j++){
-            dp[i][j] = (dp[i-1][j-1] && str1[i]==str2[j]);
+    const int str1len = str1.size();
+    const int str2len = str1.size();
+
+    int answer=0;
+    for (int i=0;i<str1len;i++){
+        int count = 0;
+        for (int j=0;i+j<str2len;j++){
+            count += str1[j]==str2[j+i];
         }
+        if (count>answer)
+            answer = count;
     }
-    for(int i=1;i<=strlen;i++){
-        if(dp[strlen-1][strlen-i] || dp[strlen-i][strlen-1]){
-            cout<<strlen-i+1<<endl;
-            break;
+    for (int i=0;i<str2len;i++){
+        int count = 0;
+        for (int j=0;i+j<str1len;j++){
+            count += str2[j]==str1[j+i];
         }
+        if (count>answer)
+            answer = count;
     }
+    cout<<answer<<endl;
     return 0;
 }
