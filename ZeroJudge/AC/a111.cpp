74444cb5bb277734bb0efcabbafcf7278cb0ea22
User-Howard <howard20060216@gmail.com>
2021-05-16 22:12:21 +0800

    DP 相關

--- ZeroJudge/AC/a111.cpp
+++ ZeroJudge/AC/a111.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
-#include<cmath>
-
+using namespace std;
 
 int main(){
     int N;
-    while(std::cin>>N && N){
-        int answer=0;
-        for(int i=0;i<N;i++)
-            answer+=pow(N-i, 2);
-        std::cout<<answer<<std::endl;
+    uint32_t dp[102]={0, 1};
+    for(int i=2;i<101;i++){
+        dp[i] = dp[i-1] + i*i;
+    }
+    while(cin>>N && N){
+        cout<<dp[N]<<endl;
     }
     return 0;
 }
