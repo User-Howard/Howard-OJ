f01397d54b8bf61b63e7e2cc1fb8d69177b8178a
User-Howard <howard20060216@gmail.com>
2021-05-17 17:31:08 +0800

    _

--- LintCode/145. Lowercase to Uppercase.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    """
-    @param character: a character
-    @return: a character
-    """
-
-    def lowercaseToUppercase(self, character):
-        # write your code here
-        return character.upper()
--- LintCode/37. Reverse 3-digit Integer.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    """
-    @param number: A 3-digit number.
-    @return: Reversed number.
-    """
-    def reverseInteger(self, number):
-        M = str(number)[::-1]
-        return int(M)
