1fadb73eb13bfe30dc3f45b4d5ebc848f3cc80bf
User-Howard <howard20060216@gmail.com>
2021-07-17 21:03:48 +0800

    Update 2. 矩陣轉換_NF.cpp

--- APCS/105 03/2. 矩陣轉換_NF.cpp
+++ APCS/105 03/2. 矩陣轉換_NF.cpp
@@ -4,20 +4,37 @@
 using namespace std;
 
 
-int R, C, M;
-vector<vector<int> > un_spin(){ // 旋轉
-    vector<vector<int> > ret(C, vector<int>(R, 0));
-    
-}
-vector<vector<int> > un_flip(){ // 翻轉
-
-}
 int main(){
+    int R, C, M;
     cin >> R >> C >> M;
-    vector<vector<int> > nums(R, vector<int>(C, 0));
+    vector<vector<int> > nums(max(R, C), vector<int>(max(R, C), 0));
+    vector<vector<int> > t(max(R, C), vector<int>(max(R, C), 0));
+    vector<int>act(M);
     for(int i=0;i<R;++i)
         for(int j=0;j<C;++j)
             cin >> nums[i][j];
-    nums = un_spin();
+    for(int i=0;i<M;++i)
+        cin >> act[i];
+    for(int c=M-1;c>=0;--c){
+        if(act[c]){
+            for(int i=0;i<R;++i)
+                for(int j=0;j<C;++j)
+                    t[i][j] = nums[R-1-i][j];
+            nums = t;
+        }
+        else{
+            for(int i=0;i<C;++i)
+                for(int j=0;j<R;++j)
+                    t[i][j] = nums[j][C-1-i];
+            nums = t;
+            swap(R, C);
+        }
+    }
+    cout << R << ' ' << C << '\n';
+    for(int i=0;i<R;++i){
+        for(int j=0;j<C;++j)
+            cout << nums[i][j] << ' ';
+        cout << endl;
+    }
     return 0;
 }
