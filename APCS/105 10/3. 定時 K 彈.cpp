ed8bee326dad056c52449e054ac31b11ce6dd756
User-Howard <howard20060216@gmail.com>
2021-07-19 13:03:57 +0800

    Update 3. 定時 K 彈.cpp

--- APCS/105 10/3. 定時 K 彈.cpp
+++ APCS/105 10/3. 定時 K 彈.cpp
@@ -7,12 +7,12 @@ int main(){
     cin.tie(NULL);
 
     int N, M, K;
-    cin>>N>>M>>K;
+    cin >> N >> M >> K;
     for(int i=0;i<N;i++)
         arr.push_back(i+1);;
     int point = 0;
     for(int i=0;i<K;i++){
-        point=(point+M-1)%arr.size();
+        point = (point+M-1)%arr.size();
         arr.erase(arr.begin()+point);
     }
     cout<<arr[(point)%arr.size()]<<endl;
