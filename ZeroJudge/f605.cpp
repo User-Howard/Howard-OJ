a89e3ba58ae62c10dd444f5f31b58547f7d32898
User-Howard <howard20060216@gmail.com>
2021-07-27 17:22:57 +0800

    風格整合

--- ZeroJudge/f605.cpp
+++ ZeroJudge/f605.cpp
@@ -8,16 +8,16 @@ const int MAX_N = 1e2+2;
 int main(){
     int N, D;
     int arr[MAX_N][3];
-    cin>>N>>D;
+    cin >> N >> D;
     for(int i=0;i<N;i++)
         for(int j=0;j<3;j++)
-            cin>>arr[i][j];
-    int count=0, money=0;
+            cin >> arr[i][j];
+    int count = 0, money = 0;
     for(int i=0;i<N;i++)
-        if(max(max(arr[i][0], arr[i][1]), arr[i][2])-min(min(arr[i][0], arr[i][1]), arr[i][2])>=D){
-            count++;
-            money+=(arr[i][0]+arr[i][1]+arr[i][2])/3;
+        if(max(max(arr[i][0], arr[i][1]), arr[i][2]) - min(min(arr[i][0], arr[i][1]), arr[i][2]) >= D){
+            ++count;
+            money += (arr[i][0] + arr[i][1] + arr[i][2]) / 3;
         }
-    cout<<count<<' '<<money<<endl;
+    cout << count << ' ' << money << endl;
     return 0;
 }
