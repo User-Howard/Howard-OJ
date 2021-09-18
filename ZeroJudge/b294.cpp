ea225bdb0e1f4bf90acc3e645b3fdff3397a16af
User-Howard <howard20060216@gmail.com>
2021-09-18 13:56:06 +0800

    for joshua

--- ZeroJudge/b294.cpp
+++ ZeroJudge/b294.cpp
@@ -1,17 +1,16 @@
-#include<iostream>
-using namespace std; 
-int main(void)
-{
+#include <iostream>
+using namespace std;
+
+
+int main(void){
 	int a;
-	while(cin>>a)
-	{
-		int i=1,doular,sum=0;
-		for (i=1;i<=a;i++)
-		{
-			cin>>doular;
-			sum+=doular*i;
+	while (cin >> a){
+		int i = 1, doular, sum = 0;
+		for (i = 1; i <= a; i++){
+			cin >> doular;
+			sum += doular * i;
 		}
-		printf("%d",sum);
+		printf("%d", sum);
 	}
 	return 0;
 }
