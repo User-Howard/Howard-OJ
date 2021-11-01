23f3b1a82a25061954ec78ba1d2686ad8b650b1e
User-Howard <howard20060216@gmail.com>
2021-11-01 08:47:01 +0800

    Update d784.cpp

--- ZeroJudge/d784.cpp
+++ ZeroJudge/d784.cpp
@@ -1,30 +1,26 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <string.h>
+#include <cmath>
 using namespace std;
 
 
-int solve(){
-    int K, m, ans=0, cnt=0;
-    cin>>K;
-    while(K--){
-        cin>>m;
-        cnt += m;
-        if(cnt<0)
-            cnt = 0;
-        else
-            ans = max(cnt, ans);
+void solve(){
+    int N, ans, nums[102], dp[102]={0};
+    cin >> N;
+    for(int i=0;i<N;++i) cin >> nums[i];
+    ans = dp[0] = nums[0];
+    for(int i=1;i<N;++i){
+        dp[i] = max(nums[i], dp[i-1]+nums[i]);
+        ans = max(ans, dp[i]);
     }
-    return ans!=0 ? ans : -1;
+    cout << ans << '\n';
+    return;
 }
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-	int N;
-	cin>>N;
-	while(N--){
-        cout<<solve()<<'\n';
-	}
+    ios::sync_with_stdio(0);cin.tie(0);
+    int K;
+    cin >> K;
+    while(K--)
+        solve();
 	return 0;
 }
-
-// finish time: 4	min, 17	sec, 72	hsec
