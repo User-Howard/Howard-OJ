887b488305d12c03ee0b95100aff41b43cd65324
User-Howard <howard20060216@gmail.com>
2021-11-13 15:59:45 +0800

    更新在bash上的語法。

--- NPSC/Judge.sh
+++ NPSC/Judge.sh
@@ -1,23 +1,26 @@
 read -p "Please enter your folder:" t_PATH
 read -p "Please enter your file(.exe):" RPOGRAM_FILE
-echo $RPOGRAM_FILE
-
+pwd
 all_correct=true
-for f in $t_PATH/*/*/*.in
+for f in $(find $t_PATH -name "*.in")
 do
-    echo "${f}"
+    echo "${f%.*}.ans"
     $RPOGRAM_FILE < $f > tmp
-    diff --side-by-side tmp ${f%.*}.ans
+    cmp -s tmp ${f%.*}.ans
     if [ $? != 0 ]; then
         all_correct=false
         break
     fi
 done
-if [ $all_correct == true ]; then
+if [ $all_correct == true ]
+then
     echo ""
-    echo "ALL PASS"
+    echo "Accept"
+    echo ""
+else
+    echo ""
+    echo "Wrong Answer"
     echo ""
 fi
-echo ""
 rm tmp
 read -p "Press ENTER to exit." enter
