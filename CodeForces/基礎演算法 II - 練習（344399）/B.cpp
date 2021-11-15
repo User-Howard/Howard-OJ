396510b5ac00d6213cd3992d115a30eb749396dc
User-Howard <howard20060216@gmail.com>
2021-11-15 08:29:20 +0800

    _

--- CodeForces/基礎演算法 II - 練習（344399）/B.cpp
+++ CodeForces/基礎演算法 II - 練習（344399）/B.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 
 int N, ans;
-int d[18]={0}, used[17]={0};
+int d[17]={0}, used[17]={0};
 bool is_prime[32] = {0, 0};
 void make_is_prime(){
     fill(is_prime+2, is_prime+32, true);
@@ -12,14 +12,13 @@ void make_is_prime(){
             for(int j=i*2;j<32;j+=i)
                 is_prime[j] = false;
 }
-void solve(int pos=1){
-    if(pos>N){
-        if(is_prime[d[N-1]])
-            ++ans;
+void solve(int pos=0){
+    if(pos>=N){
+        ++ans;
         return;
     }
     for(int i=1;i<=N;++i){
-        if(pos != 1 and (used[i] or !is_prime[d[pos - 2] + i]))
+        if(pos != 0 and (used[i] or !is_prime[d[pos - 1] + i]))
             continue;
         used[i] = true;
         d[pos] = i;
