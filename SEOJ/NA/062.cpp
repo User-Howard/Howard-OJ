35dc1398e1b225432c379bb3cdafc5e6d32c7308
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2022-03-31 11:29:37 +0800

    Update 062.cpp

--- SEOJ/NA/062.cpp
+++ SEOJ/NA/062.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 
-int arr[81+2];
 bool solved = false;
 bool same_row(int i, int j){
     return (i/9 == j/9);
@@ -22,24 +21,26 @@ int find_block(int arr[81+2]){
     return -1;
 }
 void solveSudoku(int arr[81+2]){
+    if(solved)
+        return;
     int idx = find_block(arr);
     if(idx == -1){
         solved = true;
-        for(int i=0;i<81;++i)   
+        for(int i=0;i<81;++i)
             cout << arr[i];
         cout << '\n';
     }
     else{
-        bool able_to[10]={0, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        bool can_choose[10]={0, 1, 1, 1, 1, 1, 1, 1, 1, 1};
         for(int i=0;i<81;i++){
             if(same_row(idx, i) or same_col(idx, i) or same_block(idx, i)){
-                able_to[arr[i]] = 0;
+                can_choose[arr[i]] = false;
             }
         }
         for(int i=1;i<10;i++){
             if(solved)
                 return;
-            if(!able_to[i])
+            if(!can_choose[i])
                 continue;
             arr[idx] = i;
             solveSudoku(arr);
@@ -55,12 +56,26 @@ int main(){
     while(T--){
         string s;
         cin >> s;
+        int arr[81+5];
         for(int i=0;i<81;i++)
             arr[i] = (s[i]=='.'? 0: s[i]-'0');
         solved = false;
+        for(int i=0;i<81;++i){
+            if(arr[i] == 0)
+                continue;
+            for(int j=0;j<81;++j){
+                if(i == j)
+                    continue;
+                if(!same_row(i, j) and !same_col(i, j) and !same_block(i, j))
+                    continue;
+                if(arr[i] == arr[j])
+                    goto END;
+            }
+        }
         solveSudoku(arr);
+        END:
         if(!solved)
-            cout << "No solution." << '\n'; 
+            cout << "No solution." << '\n';
     }
     return 0;
 }
