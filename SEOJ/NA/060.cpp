8cf4cf6683d537f66edb62ea7c4d1d048d686311
User-Howard <howard20060216@gmail.com>
2022-04-02 17:00:21 +0800

    ＿

--- SEOJ/NA/060.cpp
+++ SEOJ/NA/060.cpp
@@ -1,39 +1,32 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 
 const int INF = 0x3f3f3f3f;
 int A, B, C, D, M, S, T;
 int max_length = -INF, min_time = INF;
-int f(int x){
-	// int min_time = (S - t * B) / A + t + max((t * C - M) / D, 0);
-	int min_time = INF;
-	for(int t=0;t*B<=S;++t){
-		min_time = min(min_time, (S - t * B) / A + t + max((t * C - M) / D, 0));
-		cout << (S - t * B) / A + t + max((t * C - M) / D, 0) << '\n';	
-		cout << t * B + (T - t) * A + max(t * C - M, 0) / T << "\n\n";
+int f(int t){ // t秒內最多可以跑多遠？
+	// return (t * B + (T - t) * A + max(t * C - M, 0) * D);
+	int max_len = -1;
+	for(int i=0;t-i-max(i*C-M,0)/D >= 0;++i){
+		max_len = max(max_len, (i * B + (t - i - max(i * C - M, 0) / D) * A));	
 	}
-	return min_time;
+	cout << max_len << '\n';
+	return max_len;
 }
 int binary_search(int l, int r){
 	if(r - l <= 1)
-		return l;
+		return r;
 	int mid = l + (r - l) / 2;
-	if(f(mid) <= T) r = mid;
+	cout << "\n\n" << "l: " << l << '\t' << "r: " << r << '\n';
+	cout << mid << '\t' << f(mid) << '\n';
+	if(f(mid) > S) r = mid;
 	else l = mid;
 	return binary_search(l, r);
 }
 int main(){
 	cin >> A >> B >> C >> D >> M >> S >> T;
-	// cout << f(1) << '\n'; 
-	for(int t=0;t*B<=S;++t){
-		if((S - t * B) / A + t + max((t * C - M) / D, 0) >= T)
-			min_time = min(min_time, (S - t * B) / A + t + max((t * C - M) / D, 0));
-		cout << (S - t * B) / A + t + max((t * C - M) / D, 0) << '\n';
-		max_length = max(max_length, t * B + (T - t) * A + max(t * C - M, 0) / T ); 
-		cout << t * B + (T - t) * A + max(t * C - M, 0) / T << "\n\n";
-		cout << '\n';
-	}
-	cout << min_time  << '\n' << max_length << '\n';
+	cout << binary_search(M / C - 1, 300000);
 	return 0;
 }
