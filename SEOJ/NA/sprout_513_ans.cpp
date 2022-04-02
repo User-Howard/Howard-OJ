818f7b7fff87a61c7669f0c3acbb5d1f3b1b0f98
User-Howard <howard20060216@gmail.com>
2022-04-02 14:19:46 +0800

    _

--- SEOJ/NA/sprout_513_ans.cpp
+++ SEOJ/NA/sprout_513_ans.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 #include <stack>
 using namespace std;
-typedef long long LL;
-typedef pair<LL, LL> Pi;
-#pragma GCC optimize("Ofast")
-#define PB push_back
-#define MP make_pair
-#define ff first
-#define ss second
-
 struct Node
 {
     LL h, l, c;
@@ -18,26 +10,22 @@ struct Node
     }
 };
 
-int main()
-{
+int main(){
     int N;
     LL h, ans = -1;
     stack<Node> s;
     cin >> N;
     s.push(Node(0, 0, 0)); // h l c
-    for (LL c = 1; c <= N; ++c)
-    {
+    for (LL c = 1; c <= N; ++c){
         cin >> h;
-        while (s.top().h >= h)
-        {
+        while (s.top().h >= h){
             Node p = s.top();
             s.pop();
             ans = max(ans, p.h * (c - p.l));
         }
         s.push(Node(h, s.top().c + 1, c));
     }
-    while (s.top().h > 0)
-    {
+    while (s.top().h > 0){
         Node p = s.top();
         s.pop();
         ans = max(ans, p.h * (N - p.l + 1));
