c17ee2a12f8ec63a24dbfb7664e5ba5eb4659d6f
User-Howard <howard20060216@gmail.com>
2022-04-08 20:32:47 +0800

    easy

--- SEOJ/91.cpp
+++ SEOJ/91.cpp
@@ -11,17 +11,22 @@ int main(){
     cin >> T;
     while(T--){
         cin >> N;
-        int factor[10];
-        for(int i=9;i>0;--i){
+        if(N == 1){
+            cout << 1 << '\n';
+            continue;
+        }
+        int factor[10] = {0};
+        for(int i=9;i>=2;--i){
             while(N%i == 0){
                 ++factor[i];
                 N /= i;
             }
+            // cout << i << ' ' << factor[i] << '\n';
         }
         if(N != 1)
             cout << -1 << '\n';
         else{
-            for(int i=1;i<=9;++i){
+            for(int i=2;i<=9;++i){
                 for(int j=0;j<factor[i];++j)
                     cout << i;
             }
