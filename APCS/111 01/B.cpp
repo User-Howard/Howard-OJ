13f96bc45bb6a901ced147cc6294d3af3aec3fec
User-Howard <howard20060216@gmail.com>
2022-06-05 12:09:33 +0800

    medium

--- APCS/111 01/B.cpp
+++ APCS/111 01/B.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 
 int main(){
-	// ios::sync_with_stdio(0); cin.tie(0);
+	ios::sync_with_stdio(0); cin.tie(0);
 	int N, M;
 	cin >> N >> M;
 	vector<uint64_t> S(N), T(N), loss(N);
@@ -24,57 +24,40 @@ int main(){
 		v.push_back(idx-1);
 	}
 	while(v.size() != 1){
-		if(v.size()%2)
-			v1.push_back(v.back());
-		for(int i=0;(i+1)*2<=v.size();++i){
-			uint64_t a = S[v[2*i]], b = T[v[2*i]], c = S[v[2*i+1]], d = T[v[2*i+1]];
+		for(int i=0;i+1<v.size();i+=2){
+			int x = v[i], y = v[i+1];
+			uint64_t a = S[x], b = T[x], c = S[y], d = T[y];
 			uint64_t da, db, dc, dd;
-			// cout << "S: ";
-			// for(int64_t j:S)
-			// 	cout << j << ' ';
-			// cout << '\n';
-			// cout << "T: ";
-			// for(int64_t j:T)
-			// 	cout << j << ' ';
-			// cout << '\n';
 			if(a*b >= c*d){
 				da = c * d / (2 * b), db = c * d / (2 * a);
 				dc = c / 2, dd = d / 2;
-				++loss[2*i+1];
-				v1.push_back(v[2*i]);
-				if(loss[2*i + 1] < M){
-					v2.push_back(v[2*i + 1]);
+				++loss[y];
+				v1.push_back(x);
+				if(loss[y] < M){
+					v2.push_back(y);
 				}
 			}
 			else{
 				dc = a * b / (2 * d), dd = a * b / (2 * c);
 				da = a / 2, db = b / 2;
-				++loss[2*i];
-				v1.push_back(v[2*i+1]);
-				if(loss[2*i] < M){
-					v2.push_back(v[2*i]);
+				++loss[x];
+				v1.push_back(y);
+				if(loss[x] < M){
+					v2.push_back(x);
 				}
 			}
-			S[2*i] = a + da, T[2*i] = b + db;
-			S[2*i + 1] = c + dc, T[2*i + 1] = d + dd;
+			S[x] = a + da, T[x] = b + db;
+			S[y] = c + dc, T[y] = d + dd;
 		}
+		if(v.size()%2)
+			v1.push_back(v.back());
 		v = v1;
 		for(uint64_t i:v2)
 			v.push_back(i);
-		// cout << "V: ";
-		// for(int64_t j:v)
-		// 	cout << j << ' ';
-		// cout << '\n';
-		// cout << "L: ";
-		// for(int64_t j:loss)
-		// 	cout << j << ' ';
-		// cout << '\n';
-		// cout << '\n';
 		v1.clear();
 		v2.clear();
 	}
-		cout << v[0]+1 << '\n';
-	
+	cout << v[0]+1 << '\n';
 	return 0;
 }
 // 60.33
