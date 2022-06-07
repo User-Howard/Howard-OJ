3c93f6dcc5ec8965c1309b8c1ceba93dcf62aa14
User-Howard <howard20060216@gmail.com>
2022-06-07 23:38:51 +0800

    easy dag

---
+++ SEOJ/NA/165.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+using namespace std;
+
+
+int N, M;
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        cin >> N >> M;
+        priority_queue<int, vector<int>, greater<int> > que;
+        vector<int> output;
+        vector<vector<int> > G(N);
+        vector<int> cnt(N);
+        for(int a, b, i=0;i<M;++i){
+            cin >> a >> b;
+            G[a].push_back(b);
+            ++cnt[b];
+        }
+        for(int i=0;i<N;++i){
+            if(cnt[i] == 0)
+                que.push(i);
+        }
+        while(!que.empty()){
+            int p = que.top();que.pop();
+            output.push_back(p);
+            for(int i:G[p]){
+                if(--cnt[i] == 0)
+                    que.push(i);
+            }
+        }
+        if(output.size() != N)
+            cout << "QAQ" << '\n';
+        else{
+            for(int i=0;i<N;++i)
+                cout << output[i] << " \n"[i==N-1];
+        }
+    }
+    return 0;
+}
