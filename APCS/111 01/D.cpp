452ed45780fc76b4b5368aca2b225609fa346ba9
User-Howard <howard20060216@gmail.com>
2022-06-08 00:33:32 +0800

    STMP

--- APCS/111 01/D.cpp
+++ APCS/111 01/D.cpp
@@ -3,13 +3,19 @@
 /*
 */
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 
 
 int main(){
-	int N, M;
-	cin >> N >> M;
-	
+	int N, K;
+	cin >> N >> K;
+	vector<int> H(N), W(K);
+	for(int &i:H) cin >> i;
+	for(int &i:W) cin >> i;
+	int l=1, r = *max_element(H.begin(), H.end());
+	while()
 	return 0;
 }
