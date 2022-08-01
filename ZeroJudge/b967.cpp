730092219bccc9b5d1ee08e6d7bb5e75c8356816
User-Howard <howard20060216@gmail.com>
2022-08-01 21:12:47 +0800

    一年以後再寫一次

--- ZeroJudge/b967.cpp
+++ ZeroJudge/b967.cpp
@@ -1,61 +1,102 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
+#include <numeric>
 using namespace std;
 
+typedef vector<vector<int> > vvi;
+typedef vector<int> vi;
+typedef vector<pair<int, int> > vpii;
+typedef pair<int, int> pii;
+#define vE vector<E>
 
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+
+
+const int INF = 0x3f3f3f3f;
+int N;
+int answer;
 struct E {
-    int item_max = -1;
-    int item_sum = 0;
+    pii D;
+    vi G;
 };
-const int MAX_N = 1e5;
-vector<int> children [MAX_N + 2];
-E child_amount [MAX_N + 2];
-int get_max(int it) {
-    if (child_amount [it].item_max >= 0)
-        return child_amount [it].item_max;
-    else {
-        int MAX = 0;
-        for (int i = 0;i < children [it].size();i++) {
-            MAX = max(get_max(children [it][i]), MAX);
-        }
-        return child_amount [it].item_max = MAX + 1;
+vE Person;
+vi in;
+void init() {
+    answer = 0;
+    in.resize(N);
+    fill(all(in), 0);
+    Person.resize(N);
+    for(int i=0;i<N;++i)
+        Person[i].G.clear();
+}
+pii dfs(int u) {
+    if(Person[u].G.size() == 0) {
+        Person[u].D = mp(0, 0);
     }
+    else {
+        vi ct;
+        for(int i:Person[u].G) {
+            ct.pb(dfs(i).fi + 1);
+        }
+        sort(all(ct));
+        if(ct.size() >= 2)
+            Person[u].D = mp(ct[ct.size()-1], ct[ct.size()-2]);
+        else if(ct.size() == 1) 
+            Person[u].D = mp(ct[0], 0);
+        else 
+            Person[u].D = mp(0, 0);
+    }
+    answer = max(Person[u].D.fi + Person[u].D.se, answer);
+    return Person[u].D;
+}
+int solve() {
+    init();
+    for(int i=0;i<N-1;++i) {
+        int u, v;
+        cin >> u >> v;
+        Person[u].G.pb(v);
+        ++in[v];
+    }
+    int root;
+    for(int i=0;i<N;++i) {
+        if(in[i] == 0) {
+            root = i;
+            break;
+        }
+    }
+    dfs(root);
+    // for(int i=0;i<N;++i)
+    //     cout << i << '\t' << Person[i].D.fi << '\t' << Person[i].D.se << '\n';
+    return answer;
 }
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    int N;
-    while (cin >> N) {
-        for (int i = 0;i < N;i++){
-            children [i].clear();
-            child_amount[i].item_max = -1;
-            child_amount[i].item_sum = 0;
-        }
-        for (int i = 0;i < N - 1;i++) {
-            int a, b;
-            cin >> a >> b;
-            children [a].push_back(b);
-        }
-        for (int i = 0;i < N;i++) {
-            if (children [i].size() == 0)
-                child_amount [i].item_max = 0;
-        }
-        int MAX = 0;
-        for (int i = 0;i < N;i++) {
-            int m=0;
-            vector<int>vec(children[i].size());
-            for (auto j : children [i]) {
-                vec.push_back(get_max(j)+1);
-            }
-            sort(vec.begin(), vec.end());
-            if (vec.size() == 1)
-                m = max(vec[0], m);
-            else if (vec.size() >= 2)
-                m = max(vec [vec.size() - 1] + vec [vec.size() - 2]+1, m);
-            MAX = max(m-1, MAX);
-        }
-        cout<<MAX<<endl;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    while(cin >> N) {
+        cout << solve() << '\n';
     }
     return 0;
 }
