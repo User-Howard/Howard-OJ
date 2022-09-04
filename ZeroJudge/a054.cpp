a39945f274a8bde815494b5d46c6bc11b44b8932
User-Howard <howard20060216@gmail.com>
2022-09-04 13:35:54 +0800

    _

--- ZeroJudge/a054.cpp
+++ ZeroJudge/a054.cpp
@@ -6,22 +6,15 @@ int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
  
-    int li[26]={1,0,9,8,7,6,5,4,9,3,2,2,1,0,8,9,8,7,6,5,4,3,1,3,2,0};
+    string li[10] = { "BNZ" ,"AMW" ,"KLY" ,"JVX" ,"HU" ,"GT" ,"FS" ,"ER" ,"DOQ" ,"CIP"};
     int check, sum;
     string R;
-    while(cin>>R){
-        check=0;
-        sum=0;
-
-        check=R[8]-'0';
-        for(int i=0;i<8;i++)
-            sum+=(R[i]-'0')*(8-i);
-        for(int i=0;i<26;i++)
-            if(10 - (sum + li[i]) % 10 == check)
-                cout<<(char)(i+'A');
-        cout<<endl;
-        
-    }
+    cin >> R;
+    sum = 0;
+    check = R[8]-'0';
+    for(int i=0;i<8;i++)
+        sum += (R[i]-'0')*(8-i);
+    cout << li[(20 - check - (sum%10))%10] << '\n';
 
     return 0;
 }
