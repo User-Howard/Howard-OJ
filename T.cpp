3c4fe58321d9dbbb03ebef339e4a971660c1430a
User-Howard <howard20060216@gmail.com>
2022-09-17 10:00:01 +0800

    _

---
+++ T.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
+#include <numeric>
+using namespace std;
+
+typedef pair<int, int> pii;
+typedef vector<vector<int> > vvi;
+typedef vector<int> vi;
+typedef vector<pair<int, int> > vpii;
+
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string S = "abcdefg";
+    cout << S.substr(3);
+    return 0;
+}
