4b16ed2c8e96f95e8863cc3e24e050f0818ca2c7
User-Howard <howard20060216@gmail.com>
2023-02-28 18:03:14 +0800

    rename

--- CSES/1754_Coin_Piles.cpp
+++ CSES/1754_Coin_Piles.cpp
@@ -12,9 +12,9 @@ int main() {
             swap(A, B);
         int t = (2*A-B);
         if(t>=0 and t%3==0)
-        cout<<"YES\n";
+            cout<<"YES\n";
         else
-        cout<<"NO\n";
+            cout<<"NO\n";
     }
     return 0;
 }
