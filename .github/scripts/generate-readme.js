ac68f1e6c8aa3829f94a0978c0c0e8d1e8a068cb
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2023-04-09 15:30:53 +0800

    Update generate-readme.js

--- .github/scripts/generate-readme.js
+++ .github/scripts/generate-readme.js
@@ -1,11 +1,23 @@
 const fs = require('fs');
 
-const { execSync } = require('child_process');
+// 遞歸函數遍歷目錄結構，輸出到README.md中
+function generateReadme(path, level) {
+  const files = fs.readdirSync(path);
+  let output = '';
+  level = level || 0;
+  const indent = '  '.repeat(level); // 每一層的縮進
 
-const tree = execSync('tree -I node_modules').toString();
+  files.forEach(function(file) {
+    const stats = fs.statSync(path + '/' + file);
+    if (stats.isDirectory()) {
+      // 如果是資料夾，遞歸處理
+      output += indent + '- ' + file + '\n';
+      output += generateReadme(path + '/' + file, level + 1);
+    } 
+  });
 
-const readme = `# My Repository\n\n\`\`\` ${tree}\`\`\``;
+  return output;
+}
 
-console.log(readme);
-
-fs.writeFileSync('README.md', readme);
+// 輸出目錄結構到README.md中
+fs.writeFileSync('README.md', generateReadme('./'));
