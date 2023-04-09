1bf5c0d5fe80ec13275bb12499430383d33252b6
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2023-04-09 15:37:35 +0800

    Update generate-readme.js

--- .github/scripts/generate-readme.js
+++ .github/scripts/generate-readme.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 // 遞歸函數遍歷目錄結構，輸出到README.md中
-function generateReadme(path, level) {
+function generateReadme(path, level, count) {
   const files = fs.readdirSync(path);
   let output = '';
   level = level || 0;
@@ -12,22 +12,32 @@ function generateReadme(path, level) {
     if (stats.isDirectory()) {
       // 如果是資料夾，遞歸處理
       output += indent + '- ' + file + '\n';
-      output += generateReadme(path + '/' + file, level + 1);
-    } 
+      const result = generateReadme(path + '/' + file, level + 1, count);
+      output += result.output;
+      count = result.count;
+    } else {
+      // 如果是檔案，輸出檔名
+      output += indent + '- ' + file + '\n';
+      if (file.endsWith('.cpp')) {
+        count++;
+      }
+    }
   });
 
-  return output;
+  return { output: output, count: count };
 }
 
 // 生成程式碼區塊
+const result = generateReadme('./', 0, 0);
 const readmeContent = `
 # Howard-OJ
 
 ###### 專門紀錄我的程式資料夾。
 
 \`\`\` 
-${generateReadme('./')}
-\`\`\`
+${result.output}\`\`\`
+
+總共有 ${result.count} 個 .cpp 檔案。
 `;
 
 // 輸出目錄結構到README.md中
