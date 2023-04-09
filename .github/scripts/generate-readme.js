ae43c8f356e141ed05583f93d089a3f40111eda9
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2023-04-09 15:15:49 +0800

    Update generate-readme.js

--- .github/scripts/generate-readme.js
+++ .github/scripts/generate-readme.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const { execSync } = require('child_process');
 
-const tree = execSync('tree -aI node_modules').toString();
+const tree = execSync('tree -I node_modules').toString();
 
 const readme = `# My Repository\n\n\`\`\` ${tree}\`\`\``;
 
