cc2b79fab100db7979c6f6a51bbaa625fedb18d8
User-Howard <howard20060216@gmail.com>
2023-05-27 13:13:49 +0800

    基本 RMQ 分塊法

--- SEOJ/80.cpp
+++ SEOJ/80.cpp
@@ -11,8 +11,9 @@ int main(){
     int T, N;
     cin >> T >> N;
     int block_size = sqrt(N);
-    vector<int>nums((N / block_size + 1) * block_size), blocks(N/block_size + 1, INF);
-    for(int &i:nums) cin >> i;
+    vector<int>nums((N / block_size + 1) * block_size, INF), blocks(N/block_size + 1, INF);
+    for(int i=0;i<N;++i)
+        cin >> nums[i];
     for(int i=0;i<N;++i)
         blocks[i/block_size] = min(blocks[i/block_size], nums[i]);
     int c, x, y;
@@ -20,25 +21,35 @@ int main(){
         cin >> c >> x >> y;
         if(c == 1){
             int min_ele = INF;
-            int lb = x / block_size, rb = y / block_size;
-            for(int i=0;i<block_size;++i){
-                if(x <= lb*block_size+i)
-                    min_ele = min(min_ele, nums[lb*block_size+i]);
-                if(rb*block_size+i <= y)
-                    min_ele = min(min_ele, nums[rb*block_size+i]);
+            int lb = x / block_size;
+            int rb = y / block_size;
+
+            if (lb == rb) {
+                for (int i = x; i <= y; ++i)
+                    min_ele = min(min_ele, nums[i]);
             }
-            for(int i=lb+1;i<rb;++i)
-                min_ele = min(min_ele, blocks[i]);
+            else {
+                for (int i = x; i < (lb + 1) * block_size; ++i)
+                    min_ele = min(min_ele, nums[i]);
+
+                for (int i = lb + 1; i < rb; ++i)
+                    min_ele = min(min_ele, blocks[i]);
+
+                for (int i = rb * block_size; i <= y; ++i)
+                    min_ele = min(min_ele, nums[i]);
+            }
+
             cout << min_ele << '\n';
         }
         else{
+            int block_index = x / block_size;
             nums[x] = y;
-            int min_ele = INF;
-            for(int i=0;i<block_size;++i){
-                min_ele = min(min_ele, nums[x / block_size + i]);
-            }
-            blocks[x / block_size] = min_ele;
+            blocks[block_index] = INF;
+
+            for (int i = block_index * block_size; i < min((block_index + 1) * block_size, N); ++i)
+                blocks[block_index] = min(blocks[block_index], nums[i]);
         }
+        
     }
     return 0;
 }
