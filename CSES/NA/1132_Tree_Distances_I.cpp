bccc8712910dfc501032879d92e547f6e541a2ba
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2024-03-22 21:21:11 +0800

    hard 圖論、最遠距離

--- CSES/NA/1132_Tree_Distances_I.cpp
+++ CSES/NA/1132_Tree_Distances_I.cpp
@@ -1,39 +1,35 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
+#define pb push_back
+#define mp make_pair
+typedef pair<int, int> pii;
 
 const int MAX_N = 2e5 + 5;
 int N;
 vector<int> G[MAX_N];
-vector<int> child(MAX_N, 0);
-vector<int> ans(MAX_N, 0);
-int solve(int x, int pre=-1, int cnt=0) {
-    int q = 0;
+void solve(vector<int>& vec, int x, int pre=-1, int cnt=0) {
+    vec[x] = max(vec[x], cnt);
     for(int i: G[x]) {
         if(i == pre) continue;
-        q = max(solve(i, x, cnt+1), q);
+        solve(vec, i, x, cnt+1);
     }
-    ans[x] = max(cnt, q) + 1;
-    return q + 1;
 }
 int main() {
-    cout << "SO FAR SO GOOD\n";
     cin >> N;
-    for(int i=2;i<=N;++i) {
-        int a, b;
+    for(int i=2, a, b;i<=N;++i) {
         cin >> a >> b;
-        G[a].push_back(b);
-        G[b].push_back(a);
+        G[a].pb(b);
+        G[b].pb(a);
     }
-    int end_of_tree = -1;
-    for(int i=1;i<=N;++i) {
-        if(G[i].size() > 1) continue;
-        end_of_tree = i;
+    vector<int> ans(N+1);
+    int tail;
+    for(int i=0;i<3;++i) { // 1. find edge; 2,3. find answer
+        tail = distance(ans.begin(), max_element(next(ans.begin()), ans.end()));
+        solve(ans, tail);
     }
-    solve(end_of_tree);
-    cout << end_of_tree << '\n';
-    for(int i=1;i<=N;++i) cout << ans[i] << ' ';
-    cout << '\n';
+    for(int i=1;i<=N;++i)
+        cout << ans[i] << " \n"[i==N];
+    
     return 0;
 }
