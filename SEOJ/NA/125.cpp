1c3c96993297209b50d36e9be80580e16b464341
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2024-04-21 16:13:23 +0800

    _

--- SEOJ/NA/125.cpp
+++ SEOJ/NA/125.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 using namespace std;
 
-vector<int > nums, pre;
+vector<int > nums;
 const int Mod = 10000019;
 uint64_t ans = 0;
 void count(int l, int r){ // [l, r)
@@ -12,10 +12,16 @@ void count(int l, int r){ // [l, r)
     int mid = l + (r - l) / 2;
     count(l, mid); count(mid, r);
     int ci = l;
-    for(int i=mid;i<r;++i){
-        while(ci < mid and nums[ci] < nums[i]) ++ci;
-        ans += pre[mid-1] - pre[ci-1];
-        ans += mid - ci;
+    vector<uint64_t> pre(mid-l+1);
+    for(int i = l;i<mid;++i) {
+        pre[i-l+1] = pre[i-l] + nums[i];
+    }
+    for(int i = mid;i<r;++i) {
+        while(ci < mid and nums[ci] <= nums[i]) ++ci;
+        if(ci >= mid) continue;
+        ans += pre[mid-l] - pre[ci-l];
+        ans %= Mod;
+        ans += (mid-ci)*nums[i];
         ans %= Mod;
     }
     vector<int> tmp(r-l);
@@ -27,11 +33,8 @@ int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     int N;
     cin >> N;
-    nums.resize(N); pre.resize(N);
+    nums.resize(N);
     for(int &i:nums) cin >> i;
-    pre[0] = nums[0];
-    for(int i=1;i<N;++i)
-        pre[i] = pre[i-1] + nums[i];
     count(0, N);
     cout << ans << '\n';
     return 0;
