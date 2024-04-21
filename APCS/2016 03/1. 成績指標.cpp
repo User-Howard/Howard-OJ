3fb74670a0921e449dc4fdac48f56128720322dc
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2024-04-21 16:06:39 +0800

    easy _

--- APCS/2016 03/1. 成績指標.cpp
+++ APCS/2016 03/1. 成績指標.cpp
@@ -1,28 +1,27 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-
-int main(){
-    int N;
-    vector<int> nums;
-    cin >> N;
-    nums.resize(N);
-    for(int i=0;i<N;++i)
-        cin >> nums[i];
-    sort(nums.begin(), nums.end());
-    for(int i=0;i<N;++i)
-        cout << nums[i] << ' ';
-    cout << endl;
-    vector<int>::iterator it = lower_bound(nums.begin(), nums.end(), 60);
-    if(it == nums.begin())
-        cout << "best case" << endl;
-    else
-        cout << *(it-1) << endl;
-    if(it == nums.end())
-        cout << "worst case" << endl;
-    else
-        cout << *(it) << endl;
-    return 0;
+int main() {
+  int N;
+  vector<int> nums;
+  cin >> N;
+  nums.resize(N);
+  for (int i = 0; i < N; ++i)
+    cin >> nums[i];
+  sort(nums.begin(), nums.end());
+  for (int i = 0; i < N; ++i)
+    cout << nums[i] << ' ';
+  cout << endl;
+  vector<int>::iterator it = lower_bound(nums.begin(), nums.end(), 60);
+  if (it == nums.begin())
+    cout << "best case" << endl;
+  else
+    cout << *(it - 1) << endl;
+  if (it == nums.end())
+    cout << "worst case" << endl;
+  else
+    cout << *(it) << endl;
+  return 0;
 }
