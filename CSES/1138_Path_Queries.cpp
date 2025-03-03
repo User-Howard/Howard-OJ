93e494494df7eacf37ba8be65289c109cb60dfd7
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2025-03-03 21:26:53 +0800

    medium 線段樹區間

---
+++ CSES/1138_Path_Queries.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <array>
+using namespace std;
+#define eb emplace_back
+#define lowbit(x) (x&-x)
+#define int int64_t
+
+
+const int MAX_N = 2e5+5;
+
+int N, Q;
+array<vector<int>, MAX_N> G;
+array<int, MAX_N> parent;
+array<int, MAX_N> V;
+array<int, MAX_N> in;
+array<int, MAX_N> out;
+
+void dfs(int p=1, int prev=-1) {
+    static int index=1;
+    in[p] = index++;
+    for(auto i: G[p]) {
+        if(i==prev) continue;
+        dfs(i, p);
+    }
+    out[p] = index++;
+}
+array<int, MAX_N*2> BIT;
+void update(int p, int x) {
+    for(int i=p;i<=MAX_N*2;i+=lowbit(i)) {
+        BIT[i] += x;
+    }
+}
+
+int query(int p) {
+    int answer = 0;
+    for(int i=p;i>0;i-=lowbit(i)) {
+        answer += BIT[i];
+    }
+    return answer;
+}
+
+signed main(){
+    freopen("err.txt", "w", stderr);
+    cin >> N >> Q;
+    for(int i=1;i<=N;++i) {
+        cin >> V[i];
+    }
+    for(int i=1;i<N;++i) {
+        int a, b;
+        cin >> a >> b;
+        G[a].eb(b);
+        G[b].eb(a);
+    }
+    dfs();
+    for(int i=1;i<=N;++i) {
+        update(in[i], V[i]);
+        update(out[i], -V[i]);
+    }
+    int op, s, x;
+    while(Q--) {
+        cin >> op;
+        if(op==1) {
+            cin >> s >> x;
+            update(in[s], x-V[s]);
+            update(out[s],-x+V[s]);
+            V[s] = x;
+        } else {
+            cin >> s;
+            cout << query(in[s]) << '\n';
+        }
+        /*
+        for(int i=1;i<=N;++i) {
+            cerr << i << ": " << in[i] << ' ' << out[i] << ' ' << query(in[i]) << '\n';
+        }
+        for(int i=1;i<=10;++i) {
+            cerr << query(i) << '\t';
+        }
+        cerr << '\n';
+        cerr << '\n';
+        */
+    }
+ 
+    return 0;
+}
