c6900b6e1d658b91271a589893cdec8f469c8783
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2025-09-28 12:24:12 +0800

    easy trie

--- UVA/1556.cpp
+++ UVA/1556.cpp
@@ -1,53 +1,49 @@
-#include <codecvt>
+#include <deque>
 #include <iostream>
-#include <algorithm>
-#include <iterator>
-#include <vector>
-#include <string>
 #include <map>
+#include <sstream>
+#include <string>
 using namespace std;
-#define all(x) x.begin(),x.end()
 
 struct trie {
     string name;
-    map<string, trie* > mp;
-
-    trie(string s): name(s) {}
-
-    void add(vector<string> vec, int idx=0) {
-        if(idx==vec.size()) return;
-        string &S = vec[idx];
-        if(mp.find(S)==mp.end()) {
-            mp.insert(make_pair(S, new trie(vec[idx])));
-        }
-        mp[S]->add(vec, idx+1);
+    map<string, trie* > data;
+    trie(string name): name(name) {}
+    void add(deque<string>& s) {
+        if(s.empty()) return;
+        string dir = s.front(); s.pop_front();
+        if(not data[dir]) data[dir] = new trie(dir);
+        data[dir]->add(s);
     }
-    void printAll(int spacing=0) {
-        for(const auto& [dirname, chi]: mp) {
-            cout << string(spacing, ' ') << dirname << '\n';
-            chi->printAll(spacing+1);
+
+    void printAll(int indent=0) {
+        for(auto& [dir, file]: data) {
+            cout << string(indent, ' ') << dir << '\n';
+            file->printAll(indent+1);
         }
     }
 };
-int N;
-int solve() {
-    trie root("&");
-    while(N--) {
-        string path;
-        cin >> path;
-        path += '\\';
-        int last=0, pointer=0;
-        vector<string> dirs;
-        while (pointer<path.size()) {
-            while(path[pointer]!='\\') pointer++;
-            dirs.push_back(path.substr(last, pointer-last));
-            last = pointer+1;
-            pointer += 1;
-        }
-        root.add(dirs);
+deque<string> split(string& s) {
+    string token;
+    deque<string> tokens;
+    stringstream ss(s);
+
+    while(getline(ss, token, '\\')) {
+        tokens.push_back(token);
     }
-    root.printAll();
-    return 0;
+    return tokens;
+}
+
+int N;
+void solve() {
+    trie Trie("/");
+    string S;
+    while(N--) {
+        cin >> S;
+        auto splited = split(S);
+        Trie.add(splited);
+    }
+    Trie.printAll();
 }
 int main() {
     while(cin >> N) {
