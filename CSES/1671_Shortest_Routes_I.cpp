b1b08c19be2ab28b606017a3d09a1c817ca407b6
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2025-12-20 23:45:43 +0800

    easy dijastra

--- CSES/1671_Shortest_Routes_I.cpp
+++ CSES/1671_Shortest_Routes_I.cpp
@@ -1,38 +1,38 @@
+#include <functional>
 #include <iostream>
 #include <algorithm>
-#include <utility>
 #include <vector>
 #include <queue>
 using namespace std;
+#define int int64_t
 
 
-struct Cmp {
-    bool operator()(const std::pair<int, int64_t>& a, const std::pair<int, int64_t>& b) const {
-        return a.second > b.second;
-    }
-};
+using pii = pair<int, int>;
+using vpii = vector<pair<int, int> >;
 const int MAX_N = 1e5 + 10;
-const int64_t INF = 2e14 + 10;
-vector<pair<int, int64_t> > G[MAX_N];
-int64_t dp[MAX_N] = {};
-int main() {
+const int INF = 2e14 + 10;
+vpii G[MAX_N];
+int dp[MAX_N] = {};
+signed main() {
     int N, M;
     cin >> N >> M;
     for(int i=0;i<M;++i) {
         int a, b, c;
         cin >> a >> b >> c;
-        G[a].push_back(make_pair(b, c));
+        G[a].emplace_back(b, c);
     }
-    fill(dp+2, dp+N+1, INF);
-    priority_queue<pair<int, int64_t>, vector<pair<int, int64_t> >, Cmp> q;
-    q.push(make_pair(1, 0LL));
-    while(not q.empty()) {
-        pair<int, int64_t> tmp = q.top(); q.pop();
-    if(dp[tmp.first] < tmp.second) continue;
-        for(auto i: G[tmp.first]) {
-            if(tmp.second+i.second >= dp[i.first]) continue;
-            dp[i.first] = tmp.second + i.second;
-            q.push(make_pair(i.first, tmp.second + i.second));
+    fill(dp, dp+MAX_N, INF);
+    dp[1] = 0;
+    priority_queue<pii, vpii, greater<pii>> pq;
+    pq.push({0, 1});
+    while(not pq.empty()) {
+        auto [d, u] = pq.top(); pq.pop();
+        if(dp[u]!=d) continue;
+        for(auto e: G[u]) {
+            if(dp[e.first]>dp[u]+e.second) {
+                dp[e.first] = dp[u]+e.second;
+                pq.push({dp[e.first], e.first});
+            }
         }
     }
     for(int i=1;i<=N;++i)
